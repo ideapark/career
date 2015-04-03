7ae2cd93814559222dbabedb2ab29977487e433f
Zhou Peng <p@ctriple.cn>
2015-04-04 00:33:50 +0800

    process subsititution using temp file descriptor

---
+++ tmpfd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x                       # turn on execution tracing
+diff -y <(ls -lF) <(ls -l)   # run command
+set +x                       # turn off execution tracing
+
+echo ''
+echo '################## PROCESS SUBSTITUTION ####################'
+echo '                                                            '
+echo '+------+             |                                      '
+echo '|ls -lF| -----> fd 1 | fd 63 ----------+                    '
+echo '+------+            Pipe               |                    '
+echo '                                      \|/                   '
+echo '                             +----------------------------+ '
+echo '                             | diff /dev/fd/63 /dev/fd/62 | '
+echo '                             +----------------------------+ '
+echo '                                           /|\              '
+echo '+-----+             Pipe                    |               '
+echo '|ls -l| ------> fd 1 | fd 62 ---------------+               '
+echo '+-----+              |                                      '
+echo ''
