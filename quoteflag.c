f1adec3d30cff80980c409adb6f3461a7d51b254
Zhou Peng <p@ctriple.cn>
2015-04-04 09:58:04 +0800

    printf's quote flag

---
+++ quoteflag.c
@@ -0,0 +1,12 @@
+/*
+ * demonstrate printf's quote flag
+ */
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, ""); /* have to do this, or it won't work */
+    printf("%'d\n", 1234567);
+    return 0;
+}
