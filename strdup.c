81ccc140ae6c32d25eebe593f8ea8c3434687a11
Zhou Peng <p@ctriple.cn>
2015-04-04 09:04:09 +0800

    malloc() storage for a string and copy it

---
+++ strdup.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+
+/* malloc() storage for a copy of string and copy it */
+
+char *strdup(const char *str)
+{
+    size_t len;
+    char *copy;
+
+    len = strlen(str) + 1; /* include room for terminating '\0' */
+    copy = (char *)malloc(len);
+
+    if (copy != NULL)
+        strcpy(copy, str);
+
+    return copy; /* return NULL if error */
+}
