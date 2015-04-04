b922ce7fd1f6f65285d891549e36e9f86393c407
Zhou Peng <p@ctriple.cn>
2015-04-04 10:19:59 +0800

    2^n bytes data type, its address's last n bits must be 0

---
+++ padding.c
@@ -0,0 +1,17 @@
+/*
+ * DATA PADDING:
+ *
+ * 2^n bytes data type, its address's last n bits must be 0.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    char wolf[] = "like a wolf";
+    char *p = &wolf[1];
+    unsigned long l = *(unsigned long *)p;
+
+    printf("long value cast from char: %ld\n", l);
+
+    return 0;
+}
