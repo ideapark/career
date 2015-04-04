0d5070c1f8ec2e34591bb0f2d9de60bac5fdf67e
Zhou Peng <p@ctriple.cn>
2015-04-04 10:10:26 +0800

    locale-based times

---
+++ times.c
@@ -0,0 +1,24 @@
+/*
+ * demonstrate locale-based times
+ */
+#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+
+int main(void)
+{
+    char buf[100];
+    time_t now;
+    struct tm *curtime;
+
+    setlocale(LC_ALL, "");
+    time(&now);
+    curtime = localtime(&now);
+    (void)strftime(buf, sizeof(buf),
+            "It is now %A, %B %d, %Y, %I:%M %p", curtime);
+
+    printf("%s\n", buf);
+    printf("ctime() says: %s", ctime(&now));
+
+    return 0;
+}
