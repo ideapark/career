ba58376fb28f0d4eb790bf6bedf445384608d217
Zhou Peng <p@ctriple.cn>
2015-04-04 09:10:40 +0800

    fork() cp to copy file

---
+++ fork_cp.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    /* assume 2 args: source file and target file */
+    if (fork() == 0)
+        execl("/usr/bin/cp", "cp", argv[1], argv[2], (char *)0);
+    wait((int *)0);
+
+    printf("copy done\n");
+    return 0;
+}
