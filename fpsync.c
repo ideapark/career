329f09422df1197feb98a5f45c6489cd3de0ad05
Zhou Peng <p@ctriple.cn>
2015-04-04 09:24:03 +0800

    sync a stdio FILE * variable

---
+++ fpsync.c
@@ -0,0 +1,12 @@
+/* fpsync --- sync a stdio FILE * variable */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int fpsync(FILE *fp)
+{
+    if (fp == NULL || fflush(fp) == EOF || fsync(fileno(fp)) < 0)
+        return -1;
+
+    return 0;
+}
