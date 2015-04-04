a76b8d3b0f258b74e2b76b2581ba29a6826734c8
Zhou Peng <p@ctriple.cn>
2015-04-04 09:47:09 +0800

    probe default char signess

---
+++ charsign.c
@@ -0,0 +1,18 @@
+/*
+ * probe default char signess
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+     * most archetecture char is signed, range from -128 to 127
+     * but some archetecture (like ARM) char is unsigned range
+     * from 0 to 255
+     */
+    char c = -1;
+    printf("%d\n", (int)c);
+
+    return 0;
+}
