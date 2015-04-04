4d0f3280084576f96caeba3e5e5de65f0ffb8e27
Zhou Peng <p@ctriple.cn>
2015-04-04 09:24:46 +0800

    c standard library time

---
+++ ftime.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+    time_t now;
+    struct tm curtime;
+
+    time(&now);
+    curtime = *gmtime(&now);
+
+    printf("%s", ctime(&now));
+    printf("%s", asctime(&curtime));
+    printf("%s", asctime(localtime(&now)));
+
+    return 0;
+}
