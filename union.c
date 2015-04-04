cfee4262577283dbc46e396cddc7bcab8b03b48b
Zhou Peng <p@ctriple.cn>
2015-04-04 11:22:58 +0800

    union usage

---
+++ union.c
@@ -0,0 +1,18 @@
+/*
+ * brief demo of union usage.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    union i_f {
+        int i;
+        float f;
+    } u;
+
+    u.f = 12.34; /* assign a floating point value */
+    printf("%f also look like %#x\n", u.f, u.i);
+
+    exit(0);
+}
