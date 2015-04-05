5e5a3967f808924a989a50143a0ec85bbf5a2b33
Zhou Peng <p@ctriple.cn>
2015-04-06 01:03:00 +0800

    refactor

--- classic/bible/func-progarch/grep.c
+++ classic/bible/func-progarch/grep.c
@@ -21,19 +21,6 @@ main()
     return found;
 }
 
-int _getline(char s[], int lim)
-{
-    int c, i;
-
-    i = 0;
-    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
-        s[i++] = c;
-    if (c == '\n')
-        s[i++] = c;
-    s[i] = '\0';
-    return i;
-}
-
 int strindex(char s[], char t[])
 {
     int i, j, k;
