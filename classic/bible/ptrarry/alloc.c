1d1a5f29e3dcceb10487e33aed808a240e5c56a7
Zhou Peng <p@ctriple.cn>
2015-04-06 10:50:41 +0800

    pointer arithmetic
    - NULL (0) is not valid address
    - pointer can compare (not) equal to NULL (0)
    - different array pointer compare is undefined
    - p + n actually equals to p + n * (sizeof *p)
      ^^^^^                    ^^^^^^^^^^^^^^^^^^^

---
+++ classic/bible/ptrarry/alloc.c
@@ -0,0 +1,20 @@
+#define ALLOCSIZE 10000 /* avaiable space */
+
+static char allocbuf[ALLOCSIZE]; /* alloc memory area */
+static char *allocp = allocbuf;  /* next free pos */
+
+char *alloc(int n)
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n) { /* enougth free space */
+        allocp += n;
+        return allocp - n;
+    } else {
+        return 0;
+    }
+}
+
+void afree(char *p)
+{
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
