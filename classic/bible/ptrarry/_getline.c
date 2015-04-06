8792c3e00e4123f7729897578b65b695d7441373
Zhou Peng <p@ctriple.cn>
2015-04-06 12:10:00 +0800

    change soft link to hard link

--- classic/bible/ptrarry/_getline.c
+++ classic/bible/ptrarry/_getline.c
@@ -1,1 +1,14 @@
-../func-progarch/_getline.c
+#include <stdio.h>
+
+int _getline(char s[], int lim)
+{
+    int c, i;
+
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
