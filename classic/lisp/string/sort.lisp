b2e1a4de3afcdd09fc5587f5836630867b703537
Zhou Peng <p@ctriple.cn>
2015-05-23 21:31:58 +0800

    strings and characters

---
+++ classic/lisp/string/sort.lisp
@@ -0,0 +1,13 @@
+(sort "elbow" #'char<)
+
+(char "abc" 1)
+
+(equal "fred" "fred")
+
+(equal "fred" "Fred")
+
+(string-equal "fred" "Fred")
+
+(format nil "~A or ~A" "truth" "dare")
+
+(concatenate 'string "not " "to worry")
