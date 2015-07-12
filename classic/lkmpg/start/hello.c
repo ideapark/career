b9befb34ac27f47d717347a824bebbb972962760
Zhou Peng <p@ctriple.cn>
2015-07-12 22:52:43 +0800

    module init/exit function name can be user defined

--- classic/lkmpg/start/hello.c
+++ classic/lkmpg/start/hello.c
@@ -1,10 +1,12 @@
 /*
- * hello.c - The simplest kernel module.
+ * hello.c - Demonstrating the module_init() and module_exit() macros.
+ * This is preferred over using init_module() and cleanup_module().
  */
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
+#include <linux/init.h>   /* Needed for the macros */
 
-int init_module(void)
+int hello_init(void)
 {
     printk(KERN_INFO "Hello world.\n");
 
@@ -14,7 +16,10 @@ int init_module(void)
     return 0;
 }
 
-void cleanup_module(void)
+void hello_exit(void)
 {
     printk(KERN_INFO "Goodby world.\n");
 }
+
+module_init(hello_init);
+module_exit(hello_exit);
