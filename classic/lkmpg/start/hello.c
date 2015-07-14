1a2295183c03bd9a47b4814b1b0e237ac30c7bec
Zhou Peng <p@ctriple.cn>
2015-07-14 22:58:27 +0800

    the __init and __exit macros

--- classic/lkmpg/start/hello.c
+++ classic/lkmpg/start/hello.c
@@ -1,22 +1,19 @@
 /*
- * hello.c - Demonstrating the module_init() and module_exit() macros.
- * This is preferred over using init_module() and cleanup_module().
+ * hello.c - Illustrating the __init, __initdata and __exit macros.
  */
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 #include <linux/init.h>   /* Needed for the macros */
 
-int hello_init(void)
-{
-    printk(KERN_INFO "Hello world.\n");
+static int hello_data __initdata = 3;
 
-    /*
-     * A non 0 return means init_module failed; module can't be loaded.
-     */
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello world %d.\n", hello_data);
     return 0;
 }
 
-void hello_exit(void)
+static void __exit hello_exit(void)
 {
     printk(KERN_INFO "Goodby world.\n");
 }
