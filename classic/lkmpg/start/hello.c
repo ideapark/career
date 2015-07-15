f1b0159c656ac27942cda7abf4b8d374cf5b90fe
Zhou Peng <p@ctriple.cn>
2015-07-15 20:58:26 +0800

    Licensing and Module Documentation

--- classic/lkmpg/start/hello.c
+++ classic/lkmpg/start/hello.c
@@ -1,15 +1,16 @@
 /*
- * hello.c - Illustrating the __init, __initdata and __exit macros.
+ * hello.c - Demonstrates module documentation.
  */
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 #include <linux/init.h>   /* Needed for the macros */
 
-static int hello_data __initdata = 3;
+#define DRIVER_AUTHOR "Peter Jay Salzman <p@dirac.org>"
+#define DRIVER_DESC   "A simple driver"
 
 static int __init hello_init(void)
 {
-    printk(KERN_INFO "Hello world %d.\n", hello_data);
+    printk(KERN_INFO "Hello world.\n");
     return 0;
 }
 
@@ -20,3 +21,26 @@ static void __exit hello_exit(void)
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+/*
+ * You can use strings, like this:
+ */
+
+/*
+ * Get rid of taint message by declaring code as GPL.
+ */
+MODULE_LICENSE("CPL");
+
+/*
+ * Or with defines, like this:
+ */
+MODULE_AUTHOR(DRIVER_AUTHOR); /* Who wrote this module? */
+MODULE_DESCRIPTION(DRIVER_DESC); /* What does this module do */
+
+/*
+ * This module uses /dev/testdevice. The MODULE_SUPPORTED_DEVICE
+ * macro might be used in the future to help automatic configuration
+ * of modules, but is currently unused other than for documentation
+ * purpose.
+ */
+MODULE_SUPPORTED_DEVICE("testdevice");
