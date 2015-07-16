2f66235369868bb6fac6b0d75095a6bd730e0dd7
Zhou Peng <p@ctriple.cn>
2015-07-16 23:31:19 +0800

    modules spanning multiple files

---
+++ classic/lkmpg/morefile/stop.c
@@ -0,0 +1,10 @@
+/*
+ * stop.c -- Illustration of multi filed modules
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+void cleanup_module()
+{
+    printk(KERN_INFO "Short is the life of a kernel module.\n");
+}
