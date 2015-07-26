cd19c68b01f19230822afb0a05d76a88d270c293
Zhou Peng <p@ctriple.cn>
2015-07-26 10:27:18 +0800

    classic IPC problem and solution
    philosopher thinking
    readers & writer
    barbers & customers

---
+++ classic/minix/barber.c
@@ -0,0 +1,33 @@
+#define CHAIRS  5              /* chairs for waiting customers */
+
+typedef int semaphone;
+semaphone customers = 0;       /* customers waiting for barber */
+semaphone barbers = 0;         /* barbers waiting for customer */
+semaphone mutex = 1;           /* exclude access */
+int waiting = 0;               /* customers waiting (not serviced yet) */
+
+void barber(void)
+{
+    while (1) {
+        down(customers);       /* if customers is 0, sleeping */
+        down(mutex);           /* ask process wait */
+        waiting = waiting - 1; /* waiting customer minus 1 */
+        up(barbers);           /* baber starting cutting hair */
+        up(mutex);             /* release waiting */
+        cut_hair();            /* cut hair (non-critical area) */
+    }
+}
+
+void customers(void)
+{
+    down(mutex);               /* enter critical area */
+    if (waiting < CHAIRS) {
+        waiting = waiting + 1; /* waiting customer add 1 */
+        up(customers);         /* wakeup barber */
+        up(mutex);             /* release waiting access */
+        down(barbers);         /* if barbers is 0, sleeping */
+        get_haircut();         /* sit down waiting for service */
+    } else {
+        up(mutex);             /* customer leaving */
+    }
+}
