5b9054be753ca1f657160290b06029965ebc0600
Zhou Peng <p@ctriple.cn>
2015-11-18 09:27:34 +0800

    Create reporter_avltree.h

---
+++ reporter/reporter.exe/reporter_avltree.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) Zhou Peng <lockrecv@gmail.com>
+ */
+#ifndef REPORTER_AVLTREE_H
+#define REPORTER_AVLTREE_H
+
+#include <inttypes.h>
+#include <iostream>
+#include "reporter_bfdsupport.h"
+
+namespace reporter {
+
+class avl_tree
+{
+  public:
+    avl_tree();
+
+  public:
+    vma_node *leftmost(vma_node *root);
+    vma_node *rightmost(vma_node *root);
+
+  public:
+    vma_node *find(vma_node *root, uint32_t vma);
+    vma_node *insert(vma_node *&root, vma_node *node);
+    vma_node *rmnode(vma_node *&root);
+    void debug_print(const vma_node *root) const;
+
+  private:
+    int height(vma_node *root);
+    int diff(vma_node *root);
+    vma_node *rr_rotation(vma_node *parent);
+    vma_node *ll_rotation(vma_node *parent);
+    vma_node *lr_rotation(vma_node *parent);
+    vma_node *rl_rotation(vma_node *parent);
+    vma_node *balance(vma_node *root);
+
+  public:
+    vma_node *root_node;
+};
+
+} /* namespace reporter */
+
+#endif /* REPORTER_AVLTREE_H */
