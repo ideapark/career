7cc441efa0d08a7b6701e492131b989c631fc2ef
Zhou Peng <p@ctriple.cn>
2015-11-18 09:28:30 +0800

    Create reporter_bfdsupport.h

---
+++ reporter/reporter.exe/reporter_bfdsupport.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) Zhou Peng <lockrecv@gmail.com>
+ */
+#ifndef REPORTER_BFDSUPPORT_H
+#define REPORTER_BFDSUPPORT_H
+
+#include <string>
+#include "op_bfd.h"
+
+namespace reporter {
+
+class module_node {
+  public:
+    uint32_t vma_begin;
+    uint32_t vma_end;
+    int bfd_key;
+
+    std::string unix_path;
+};
+
+class vma_node {
+  public:
+    uint32_t vma_begin;
+    uint32_t vma_end;
+    int bfd_key;
+
+    std::string unix_path;
+
+    vma_node *left;
+    vma_node *right;
+};
+
+class bfd_node {
+  public:
+    bool is_sre;
+    std::string win_path;
+    op_bfd *pbfd; /* this member must be 'new'ed, NOT 'malloc'ed */
+
+  public:
+    virtual ~bfd_node() {
+      if (pbfd != 0) {
+        delete pbfd;
+        pbfd = 0;
+      }
+    }
+};
+
+class key_path {
+  public:
+    int bfd_key;
+    std::string win_path;
+};
+
+class sym_info {
+  public:
+    int bfd_key;
+    uint32_t sym_index;
+    uint32_t vma_start;
+};
+
+} /* namespace reporter */
+
+#endif /* REPORTER_BFDSUPPORT_H */
