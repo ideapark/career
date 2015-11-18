4fc7784399ee4a3806302a1be96f22823b8214e3
Zhou Peng <p@ctriple.cn>
2015-11-18 10:01:25 +0800

    Create reporter_sqlite.h

---
+++ reporter/reporter.exe/reporter_sqlite.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) Zhou Peng <lockrecv@gmail.com>
+ */
+#ifndef REPORTER_SQLITE_H
+#define REPORTER_SQLITE_H
+
+#include <string>
+#include <sqlite3.h>
+
+namespace reporter {
+
+typedef enum {
+  F_CLOSED = 0,
+  F_OPEN   = 1,
+} openflag_t;
+
+typedef struct {
+  openflag_t status;
+  sqlite3 *sqlitedb;
+} sqlite_t;
+
+/*
+ * sqlite_open -- open database and begin transaction
+ */
+int sqlite_open(sqlite_t *database, const char *name);
+
+/*
+ * sqlite_exec -- execute sql statement
+ */
+int sqlite_exec(sqlite_t *database, const char *sql);
+
+/*
+ * sqlite_close -- close database and commit transaction
+ */
+int sqlite_close(sqlite_t *database);
+
+} /* namespace reporter */
+
+#endif /* REPORTER_SQLITE_H */
