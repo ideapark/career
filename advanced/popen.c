3f1121589bf708be259845139a1496e95553b1be
Zhou Peng <p@ctriple.cn>
2015-12-14 20:50:33 +0800

    Using popen

---
+++ advanced/popen.c
@@ -0,0 +1,17 @@
+/*
+ * Example Using popen
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+  FILE *stream = popen("sort", "w");
+  fprintf(stream, "This is a test.\n");
+  fprintf(stream, "Hello, world.\n");
+  fprintf(stream, "My dog has fleas.\n");
+  fprintf(stream, "This program is great.\n");
+  fprintf(stream, "On fish, two fish.\n");
+  return pclose(stream);
+}
