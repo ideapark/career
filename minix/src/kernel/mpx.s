819d89b960000c91e521dcfd0f7ab2a61cbafc3e
Zhou Peng <p@ctriple.cn>
2016-02-13 20:07:39 +0800

    kernel/mpx386.s

--- minix/src/kernel/mpx.s
+++ minix/src/kernel/mpx.s
@@ -1,4 +1,5 @@
-#! Chooses between the 8086 and 386 versions of the Minix startup code.
+#
+! Chooses between the 8086 and 386 versions of the Minix startup code.
 
 #include <minix/config.h>
 #if _WORD_SIZE == 2
