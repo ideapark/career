e033331fd95abe5c99a52c15ffd819307f56be24
Zhou Peng <p@ctriple.cn>
2016-03-06 22:57:35 +0800

    Print out shortest path (Buggy)

--- algorithm/dijkstra/dijkstra.cc
+++ algorithm/dijkstra/dijkstra.cc
@@ -1,10 +1,12 @@
 /**
  * Copyright (C) Zhou Peng <lockrecv@gmail.com>
  */
-#include <vector>
-#include <stack>
-#include <map>
 #include <set>
+#include <map>
+#include <stack>
+#include <vector>
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
@@ -12,9 +14,9 @@ typedef unsigned int Vertex;
 typedef unsigned int Weight;
 
 typedef struct {
-  Vertex src;
-  Vertex dst;
-  Weight weight;
+    Vertex src;
+    Vertex dst;
+    Weight weight;
 } Edge;
 
 static vector<Edge> Graph;
@@ -24,69 +26,98 @@ typedef set<Vertex>::iterator VertexIter;
 
 void Dijkstra(vector<Edge> &Graph, Vertex src)
 {
-  map<Vertex, Weight> Distance;
-  map<Vertex, Vertex> Previous;
-  set<Vertex> Q;
+    map<Vertex, Weight> Distance;
+    map<Vertex, Vertex> Previous;
+    set<Vertex> Q;
 
-  for (EdgeIter iter = Graph.begin(); iter != Graph.end(); iter++) {
-    Distance[iter->src] = ~0;
-    Distance[iter->dst] = ~0;
+    for (EdgeIter iter = Graph.begin(); iter != Graph.end(); iter++) {
+        Distance[iter->src] = ~0;
+        Distance[iter->dst] = ~0;
 
-    Previous[iter->src] = ~0;
-    Previous[iter->dst] = ~0;
+        Previous[iter->src] = ~0;
+        Previous[iter->dst] = ~0;
 
-    Q.insert(Q.begin(), iter->src);
-    Q.insert(Q.begin(), iter->dst);
-  }
-
-  Distance[src] = 0;
-
-  while (!Q.empty()) {
-    VertexIter minIter = Q.begin();
-
-    for (VertexIter iter = Q.begin(); iter != Q.end(); iter++) {
-      if (Distance[*iter] < Distance[*minIter])
-        minIter = iter;
+        Q.insert(Q.begin(), iter->src);
+        Q.insert(Q.begin(), iter->dst);
     }
 
-    Vertex u = *minIter;
+    Distance[src] = 0;
 
-    Q.erase(minIter);
+    while (!Q.empty()) {
 
-    for (EdgeIter iter = Graph.begin(); iter != Graph.end(); ++iter) {
-      if (iter->src == u) {
-        Weight alt = Distance[u] + iter->weight;
-        if (alt < Distance[iter->dst]) {
-          Distance[iter->dst] = alt;
-          Previous[iter->dst] = u;
+        VertexIter minIter = Q.begin();
+
+        for (VertexIter iter = Q.begin(); iter != Q.end(); iter++) {
+            if (Distance[*iter] < Distance[*minIter])
+                minIter = iter;
         }
-      }
-    }
 
-  }
+        Vertex u = *minIter;
+
+        // Print out shortest path
+        {
+            stack<Vertex> S;
+            Vertex backVertex = u;
+
+            while (Previous[backVertex] != ~0) {
+                S.push(backVertex);
+                backVertex = Previous[backVertex];
+            }
+            S.push(backVertex);
+
+            cout << " T: " << setw(3) << u
+                 << " D: " << setw(10) << Distance[u]
+                 << " P: ";
+            while (!S.empty()) {
+                cout << setw(3) << S.top() << " -> ";
+                S.pop();
+            }
+            cout << endl;
+        }
+
+        Q.erase(minIter);
+
+        for (EdgeIter iter = Graph.begin(); iter != Graph.end(); ++iter) {
+            if (iter->src == u) {
+                Weight alt = Distance[u] + iter->weight;
+                if (alt < Distance[iter->dst]) {
+                    Distance[iter->dst] = alt;
+                    Previous[iter->dst] = u;
+                }
+            }
+        }
+
+    }
 
 }
 
 int main(int argc, char *argv[])
 {
-  Edge testEdges[] = {
-    {1,2,10},
-    {1,3,20},
-    {1,4,15},
-    {2,5,13},
-    {2,6,1},
-    {2,7,8},
-    {3,8,10},
-    {3,9,20},
-    {3,10,15},
-    {3,12,1},
-    {4,13,12},
-  };
+    Edge testEdges[] = {
+        {1,2,10},
+        {1,3,20},
+        {1,4,15},
+        {2,5,13},
+        {2,6,1},
+        {2,7,8},
+        {3,8,10},
+        {3,9,20},
+        {3,10,15},
+        {3,12,1},
+        {4,13,12},
+    };
 
-  for (int i = 0; i < sizeof(testEdges)/sizeof(testEdges[0]); i++)
-    Graph.push_back(testEdges[i]);
+    cout << "==Graph==" << endl;
 
-  Dijkstra(Graph, 2);
+    for (int i = 0; i < sizeof(testEdges)/sizeof(testEdges[0]); i++) {
+        Graph.push_back(testEdges[i]);
+        cout << " s: " << testEdges[i].src
+             << " d: " << testEdges[i].dst
+             << " w: " << testEdges[i].weight
+             << endl;
+    }
 
-  return 0;
+    Dijkstra(Graph, 2);
+
+    return 0;
 }
