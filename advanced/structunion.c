218c3fe0f4e2700e4bb1c37229326c3a83ae6624
Zhou Peng <p@ctriple.cn>
2016-03-24 19:05:47 +0800

    Create structunion.c

---
+++ advanced/structunion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct point {
+    int x;
+    int y;
+    int z;
+};
+
+int main(int argc, char *argv[])
+{
+    struct point p = {.x=3, .y=4, .z=5};
+
+    printf("POINT(x=%d, y=%d, z=%d)\n", p.x, p.y, p.z);
+
+    return 0;
+}
