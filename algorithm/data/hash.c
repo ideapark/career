b62dde67fb7a412b6a198e343f43aa2fa6b878bd
Zhou Peng <p@ctriple.cn>
2016-04-07 20:08:52 +0800

    Hash table

---
+++ algorithm/data/hash.c
@@ -0,0 +1,47 @@
+#define NHASH   517  /* primer number is prefered */
+
+typedef struct Nameval Nameval;
+struct Nameval {
+    char    *name;
+    int     value;
+    Nameval *next;  /* in chain */
+};
+
+Nameval *symtab[NHASH]; /* a symbol table */
+
+/* lookup: find name in symtab, with optional create */
+Nameval *lookup(char *name, int create, int value)
+{
+    int h;
+    Nameval *sym;
+
+    h = hash(name);
+    for (sym = symtab[h]; sym != NULL; sym = sym->next)
+        if (strcmp(name, sym->name) == 0)
+            return sym;
+
+    if (create) {
+        sym = (Nameval *) emalloc(sizeof(Nameval));
+        sym->name = name;  /* assumed allocated elsewhere */
+        sym->value = value;
+        sym->next = symtab[h];
+        symtab[h] = sym;
+    }
+
+    return sym;
+}
+
+enum { MULTIPLIER = 31 };
+
+/* hash: compute hash value of string */
+unsigned int hash(char *str)
+{
+    unsigned int h;
+    unsigned char *p;
+
+    h = 0;
+    for (p = (unsigned char *) str; *p != '\0'; p++)
+        h = MULTIPLIER * h + *p;
+
+    return h % NHASH;
+}
