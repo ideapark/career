182e1518cf4626aff51d6b09a68856111516c38f
Zhou Peng <p@ctriple.cn>
2016-04-10 22:22:02 +0800

    Sizeof primitive types

---
+++ algorithm/style/sizeof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* sizeof: display sizes of basic types */
+int main(void)
+{
+    printf("char %lu, short %lu, int %lu, long %lu,",
+            sizeof(char), sizeof(short),
+            sizeof(int), sizeof(long));
+    printf(" float %lu, double %lu, void* %lu\n",
+            sizeof(float), sizeof(double), sizeof(void *));
+    return 0;
+}
