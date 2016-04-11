d90db84301e14f0b3beace3eb5b0d98d27bcaa20
Zhou Peng <p@ctriple.cn>
2016-04-11 22:04:18 +0800

    unpack

--- algorithm/style/pack.c
+++ algorithm/style/pack.c
@@ -40,3 +40,81 @@ int pack(uchar *buf, char *fmt, ...)
     va_end(args);
     return bp - buf;
 }
+
+/* pack_type1: pack format 1 packet */
+int pack_type1(uchar *buf, ushort count, uchar val, ulong data)
+{
+    return pack(buf, "cscl", 0x01, count, val, data);
+}
+
+/* unpack: unpack packed items for buf, return length */
+int unpack(uchar *buf, char *fmt, ...)
+{
+    va_list args;
+    char *p;
+    uchar *bp, *pc;
+    ushort *ps;
+    ulong *pl;
+
+    bp = buf;
+    va_start(args, fmt);
+    for (p = fmt; *p != '\0'; p++) {
+        switch (*p) {
+        case 'c':  /* char */
+            pc = va_arg(args, uchar);
+            *pc = *bp++;
+            break;
+        case 's':  /* short */
+            ps = va_arg(args, ushort*);
+            *ps = *bp++ << 8;
+            *ps |= *bp++;
+            break;
+        case 'l':  /* long */
+            pl = va_arg(args, ulong);
+            *pl = *bp++ << 24;
+            *pl |= *bp++ << 16;
+            *pl |= *bp++ << 8;
+            *pl |= *bp++;
+            break;
+        default:   /* illegal type */
+            va_end(args);
+            return -1;
+        }
+    }
+    va_end(args);
+    return bp - buf;
+}
+
+/* unpack_type2: unpack and process type 2 packet */
+int unpack_type2(int n, uchar *buf)
+{
+    uchar c;
+    ushort count;
+    ulong dw1, dw2;
+
+    if (unpack(buf, "csll", &c, &count, &dw1, &dw2) != n)
+        return -1;
+    assert(c == 0x02);
+    return process_type2(count, dw1, dw2);
+}
+
+int (*unpackfn[])(int, uchar *) = {
+    unpack_type0,
+    unpack_type1,
+    unpack_type2,
+};
+
+/* receive: read packets from network, process them */
+void receive(int network)
+{
+    uchar type, buf[BUFSIZ];
+    int n;
+
+    while ((n = readpacket(network, buf, BUFSIZ)) > 0) {
+        type = buf[0];
+        if (type >= NELEMS(unpackfn))
+            eprintf("bad packet type 0x%x", type);
+        if ((*unpackfn[type])(n, buf) < 0)
+            eprintf("protocol error, type %x length %d", type, n);
+    }
+}
