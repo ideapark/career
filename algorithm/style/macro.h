642dde02ad5b273360240a6968341fa956a73344
Zhou Peng <p@ctriple.cn>
2016-04-12 22:03:13 +0800

    Generating code using macro

---
+++ algorithm/style/macro.h
@@ -0,0 +1,11 @@
+#define LOOP(CODE) {               \
+    t0 = clock();                  \
+    for (i = 0; i < n; i++) {      \
+        CODE;                      \
+    }                              \
+    printf("%7d ", clock() - t0);  \
+}
+
+LOOP(f1 = f2)
+LOOP(f1 = f2 + f3)
+LOOP(f1 = f2 - f3)
