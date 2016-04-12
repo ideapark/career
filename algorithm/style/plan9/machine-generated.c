7f44b84b6f19bde3685d9ad96db027278fb7cd3d
Zhou Peng <p@ctriple.cn>
2016-04-12 14:58:11 +0800

    plan9: program that writes program

---
+++ algorithm/style/plan9/machine-generated.c
@@ -0,0 +1,10 @@
+/* machine-generated; do not edit. */
+
+char *errs[] = {
+    "Permission denied",     /* Eperm */
+    "I/O error",             /* Eio */
+    "File does not exist",   /* Efile */
+    "Memory limit reached",  /* Emem */
+    "Out of file space",     /* Espace */
+    "It's all Greg's fault", /* Egreg */
+};
