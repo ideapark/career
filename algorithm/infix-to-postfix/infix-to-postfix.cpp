b0adb4f70ba99be01286c556a3fe5243d7255fe4
Zhou Peng <p@ctriple.cn>
2016-04-18 00:25:36 +0800

    Infix to postfix expression implementation

---
+++ algorithm/infix-to-postfix/infix-to-postfix.cpp
@@ -0,0 +1,75 @@
+/*
+ * Infix to postfix expression
+ */
+#include <stack>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+#define IS_ADD(ch)       ((ch) == "+")
+#define IS_MINUS(ch)     ((ch) == "-")
+#define IS_MULTIPLY(ch)  ((ch) == "*")
+#define IS_DIVIDE(ch)    ((ch) == "/")
+
+#define IS_OPERATOR(ch)  (IS_ADD(ch) || IS_MINUS(ch) || IS_MULTIPLY(ch) || IS_DIVIDE(ch))
+#define PRECEDENCE_HIGHER(op1, op2)  (op1 == op2)
+
+string infixToPostfix(string expression)
+{
+    ostringstream infixExpressoinWriter;
+
+    for (int i = 0; i < expression.length(); i++) {
+        if (IS_OPERATOR(expression.substr(i, 1)))
+            infixExpressoinWriter << " " << expression.substr(i, 1) << " ";
+        else
+            infixExpressoinWriter << expression.substr(i, 1);
+    }
+
+    string token;
+    stack<string> operatorStack;
+    istringstream infixExpressionReader(infixExpressoinWriter.str());
+    ostringstream postfixExpressionWriter;
+
+    while (infixExpressionReader >> token) {
+        if (IS_OPERATOR(token)) {
+            while (!operatorStack.empty()) {
+                const string &topOperator = operatorStack.top();
+                if (PRECEDENCE_HIGHER(token, topOperator)) {
+                    break;
+                } else {
+                    postfixExpressionWriter << " " << topOperator << " ";
+                    operatorStack.pop();    
+                }
+            }
+            operatorStack.push(token);
+        } else {
+            postfixExpressionWriter << " " << token << " ";
+        }
+    }
+
+    while (!operatorStack.empty()) {
+        const string &topOperator = operatorStack.top();
+        postfixExpressionWriter << " " << topOperator << " ";
+        operatorStack.pop();
+    }
+
+    return postfixExpressionWriter.str();
+}
+
+#define TEST(expression)  do {                   \
+    cout << expression << ": ";                  \
+    cout << infixToPostfix(expression) << endl;  \
+} while (0)
+
+int main(void)
+{
+    TEST("12 + 2 * 8 - 9");
+    TEST("12 / 2 * 8 + 9");
+    TEST("12 / 2 * 8 - 9");
+    TEST("12 + 2 * 8 + 9");
+    TEST("12 * 2 * 8 * 9");
+
+    return 0;
+}
