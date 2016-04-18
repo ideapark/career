75c29023a2fc331f439d59def53ac038673e5e1a
Zhou Peng <p@ctriple.cn>
2016-04-18 22:08:12 +0800

    Gcc weak reference attribute

---
+++ linker/weakref.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Zhou Peng <lockrecv@qq.com>
+ *
+ * Compile this source with or without libpthread, and
+ * you will unstandard what it is saying.
+ *
+ *     gcc -Wall weakref.c
+ *
+ *     gcc -Wall weakref.c -lpthread
+ */
+#include <stdio.h>
+#include <pthread.h>
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+        void *(*start_routine)(void *), void *arg) __attribute__((weak));
+
+int main(void)
+{
+    if (pthread_create) {
+        printf("This is multi-thread version.\n");
+    } else {
+        printf("This is single-thread version.\n");
+    }
+
+    return 0;
+}
