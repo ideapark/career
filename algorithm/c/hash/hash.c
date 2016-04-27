8051694fab0d26ebce7092de2e986cbe2b76cb56
Zhou Peng <p@ctriple.cn>
2016-04-27 22:00:40 +0800

    Write good hash function

---
+++ algorithm/c/hash/hash.c
@@ -0,0 +1,11 @@
+typedef unsigned int Index;
+
+Index Hash(const char *Key, int TableSize)
+{
+    unsigned int HashVal = 0;
+
+    while (*Key != '\0')
+        HashVal += (HashVal << 5) + *Key++;
+
+    return HashVal % TableSize;
+}
