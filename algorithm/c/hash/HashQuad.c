fba2b6a067ce35899e01b66b6dccc335f563859c
Zhou Peng <p@ctriple.cn>
2016-05-02 11:46:36 +0800

    Rehashing

--- algorithm/c/hash/HashQuad.c
+++ algorithm/c/hash/HashQuad.c
@@ -71,3 +71,24 @@ void Insert(ElementType Key, HashTable H)
         H->TheCells[Pos].Element = Key;
     }
 }
+
+HashTable Rehash(HashTable H)
+{
+    int i, OldSize;
+    Cell *OldCells;
+
+    OldCells = H->TheCells;
+    OldSize = H->TableSize;
+
+    /* Get a new, empty table */
+    H = InitializeTable(2 * OldSize);
+
+    /* Scan through old table, reinserting into new */
+    for (i = 0; i < OldSize; i++)
+        if (OldCells[i].Info == Legitimate)
+            Insert(OldCells[i].Element, H);
+
+    free(OldCells);
+
+    return H;
+}
