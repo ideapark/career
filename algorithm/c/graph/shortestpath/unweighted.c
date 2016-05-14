c2e09fbda1ceadbbeb6102b9dcb72bd2dfb63810
Zhou Peng <p@ctriple.cn>
2016-05-14 22:41:17 +0800

    Graph: unweighted shortest path

---
+++ algorithm/c/graph/shortestpath/unweighted.c
@@ -0,0 +1,20 @@
+void Unweighted(Table T)  /* Assume t is initialized */
+{
+    int CurrDist;
+    Vertex V, W;
+
+    for (CurrDist = 0; CurrDist < NumVertex; CurrDist++) {
+        for each vertex V {
+            if (!T[V].Known && T[V].Dist == CurrDist) {
+                T[V].Known = True;
+                for each W adjacent to V
+                    if (T[W].Dist == Infinity) {
+                        T[W].Dist = CurrDist + 1;
+                        T[W].Path = V;
+                    }
+            }
+
+        }
+
+    }
+}
