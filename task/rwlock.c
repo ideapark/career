a1bb3f7b33d83ff6c8a7fe175a49f5e041be3c94
Zhou Peng <p@ctriple.cn>
2016-06-01 09:22:57 +0800

    Thread synchronization: read/write lock

---
+++ task/rwlock.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+
+int shared;
+pthread_rwlock_t lock;
+
+void *thread_function(void *arg)
+{
+    pthread_rwlock_rdlock(&lock);
+    /* Read from the shared resource */
+    pthread_rwlock_unlock(&lock);
+    return NULL;
+}
+
+int main(void)
+{
+    pthread_rwlock_init(&lock, NULL);
+
+    /* Start threads here */
+
+    pthread_rwlock_wrlock(&lock);
+    /* Write to the shared resource */
+    pthread_rwlock_unlock(&lock);
+
+    /* Join with threads here */
+
+    pthread_rwlock_destroy(&lock);
+    return 0;
+}
