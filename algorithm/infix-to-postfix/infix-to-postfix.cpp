1391954672298bf6c373a25d02c4e3cd559b9367
Zhou Peng <p@ctriple.cn>
2016-06-26 00:09:34 +0800

    Revised

--- algorithm/infix-to-postfix/infix-to-postfix.cpp
+++ algorithm/infix-to-postfix/infix-to-postfix.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Zhou Peng <lockrecv@qq.com> 
+ * Copyright (C) Zhou Peng <lockrecv@qq.com>
  *
  * Infix to postfix expression
  */
@@ -23,110 +23,110 @@ using namespace std;
 
 string infixToPostfix(const string expression)
 {
-    ostringstream infixExpressoinWriter;
+	ostringstream infixExpressoinWriter;
 
-    for (int i = 0; i < expression.length(); i++) {
-        if (IS_OPERATOR(expression.substr(i, 1)))
-            infixExpressoinWriter << " " << expression.substr(i, 1) << " ";
-        else
-            infixExpressoinWriter << expression.substr(i, 1);
-    }
+	for (int i = 0; i < expression.length(); i++) {
+		if (IS_OPERATOR(expression.substr(i, 1)))
+			infixExpressoinWriter << " " << expression.substr(i, 1) << " ";
+		else
+			infixExpressoinWriter << expression.substr(i, 1);
+	}
 
-    string token;
-    stack<string> operatorStack;
-    istringstream infixExpressionReader(infixExpressoinWriter.str());
-    ostringstream postfixExpressionWriter;
+	string token;
+	stack<string> operatorStack;
+	istringstream infixExpressionReader(infixExpressoinWriter.str());
+	ostringstream postfixExpressionWriter;
 
-    while (infixExpressionReader >> token) {
-        if (IS_OPERATOR(token)) {
-            while (!operatorStack.empty()) {
-                const string &topOperator = operatorStack.top();
-                if (PRECEDENCE_HIGHER(token, topOperator)) {
-                    break;
-                } else {
-                    postfixExpressionWriter << " " << topOperator << " ";
-                    operatorStack.pop();    
-                }
-            }
-            operatorStack.push(token);
-        } else {
-            postfixExpressionWriter << " " << token << " ";
-        }
-    }
+	while (infixExpressionReader >> token) {
+		if (IS_OPERATOR(token)) {
+			while (!operatorStack.empty()) {
+				const string &topOperator = operatorStack.top();
+				if (PRECEDENCE_HIGHER(token, topOperator)) {
+					break;
+				} else {
+					postfixExpressionWriter << " " << topOperator << " ";
+					operatorStack.pop();
+				}
+			}
+			operatorStack.push(token);
+		} else {
+			postfixExpressionWriter << " " << token << " ";
+		}
+	}
 
-    while (!operatorStack.empty()) {
-        const string &topOperator = operatorStack.top();
-        postfixExpressionWriter << " " << topOperator << " ";
-        operatorStack.pop();
-    }
+	while (!operatorStack.empty()) {
+		const string &topOperator = operatorStack.top();
+		postfixExpressionWriter << " " << topOperator << " ";
+		operatorStack.pop();
+	}
 
-    return postfixExpressionWriter.str();
+	return postfixExpressionWriter.str();
 }
 
 static string doCalculation(const string leftOperand,
-                            const string rightOperand,
-                            const string operatorString)
+		const string rightOperand,
+		const string operatorString)
 {
-    unsigned int left = atoi(leftOperand.c_str());
-    unsigned int right = atoi(rightOperand.c_str());
-    unsigned int result = 0;
+	unsigned int left = atoi(leftOperand.c_str());
+	unsigned int right = atoi(rightOperand.c_str());
+	unsigned int result = 0;
 
-    if (IS_ADD(operatorString))
-        result = left + right;
+	if (IS_ADD(operatorString))
+		result = left + right;
 
-    if (IS_MINUS(operatorString))
-        result = left - right;
+	if (IS_MINUS(operatorString))
+		result = left - right;
 
-    if (IS_MULTIPLY(operatorString))
-        result = left * right;
+	if (IS_MULTIPLY(operatorString))
+		result = left * right;
 
-    if (IS_DIVIDE(operatorString))
-        result = left / right;
+	if (IS_DIVIDE(operatorString))
+		result = left / right;
 
-    ostringstream stringWriter;
+	ostringstream stringWriter;
 
-    stringWriter << result;
+	stringWriter << result;
 
-    return stringWriter.str();
+	return stringWriter.str();
 }
 
 string calcPostfixExpression(const string postfixExpression)
 {
-    string token;
-    stack<string> calculationStack;
-    istringstream postfixExpressionReader(postfixExpression);
+	string token;
+	stack<string> calculationStack;
+	istringstream postfixExpressionReader(postfixExpression);
 
-    while (postfixExpressionReader >> token) {
-        if (IS_OPERATOR(token)) {
-            const string rightOperand = calculationStack.top();
-            calculationStack.pop();
-            const string leftOperand = calculationStack.top();
-            calculationStack.pop();
+	while (postfixExpressionReader >> token) {
+		if (IS_OPERATOR(token)) {
+			const string rightOperand = calculationStack.top();
+			calculationStack.pop();
+			const string leftOperand = calculationStack.top();
+			calculationStack.pop();
 
-            string result = doCalculation(leftOperand, rightOperand, token);
-            calculationStack.push(result);
-        } else {
-            calculationStack.push(token);
-        }
-    }
+			string result = doCalculation(leftOperand, rightOperand, token);
+			calculationStack.push(result);
+		} else {
+			calculationStack.push(token);
+		}
+	}
 
-    return calculationStack.top();
+	return calculationStack.top();
 }
 
-#define TEST(expression)  do {                                 \
-    string postfixExpression = infixToPostfix(expression);     \
-    cout << expression << ": ";                                \
-    cout << postfixExpression  << ": ";                        \
-    cout << calcPostfixExpression(postfixExpression) << endl;  \
+#define TEST(expression)  do { \
+	string postfixExpression = infixToPostfix(expression); \
+	cout << expression << ": "; \
+	cout << postfixExpression  << ": "; \
+	cout << calcPostfixExpression(postfixExpression) << endl; \
 } while (0)
 
 int main(void)
 {
-    TEST("12 + 2 * 8 - 9");
-    TEST("12 / 2 * 8 + 9");
-    TEST("12 / 2 * 8 - 9");
-    TEST("12 + 2 * 8 + 9");
-    TEST("12 * 2 * 8 * 9");
+	TEST("12 + 2 * 8 - 9");
+	TEST("12 / 2 * 8 + 9");
+	TEST("12 / 2 * 8 - 9");
+	TEST("12 + 2 * 8 + 9");
+	TEST("12 * 2 * 8 * 9");
 
-    return 0;
+	return 0;
 }
