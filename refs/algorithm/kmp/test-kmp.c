55aca85d3735ebb6c679d69c3d316372d2e5aff5
Zhou Peng <p@ctriple.cn>
2016-07-10 00:07:35 +0800

    More test cases

--- refs/algorithm/kmp/test-kmp.c
+++ refs/algorithm/kmp/test-kmp.c
@@ -2,16 +2,27 @@
 
 int main(void)
 {
-	const char *origin = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const char *expect1 = "opqrst";
-	const char *expect2 = "abcfik";
-	const char *expect3 = "ABCDEF";
-	const char *expect4 = "ABCFIK";
+	const char *origin1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char *expect11 = "opqrst";
+	const char *expect12 = "abcfik";
+	const char *expect13 = "ABCDEF";
+	const char *expect14 = "ABCFIK";
 
-	assert(kmp_search(origin, expect1) == 14);
-	assert(kmp_search(origin, expect2) == -1);
-	assert(kmp_search(origin, expect3) == 26);
-	assert(kmp_search(origin, expect4) == -1);
+	assert(kmp_search(origin1, expect11) == 14);
+	assert(kmp_search(origin1, expect12) == -1);
+	assert(kmp_search(origin1, expect13) == 26);
+	assert(kmp_search(origin1, expect14) == -1);
+
+	const char *origin2 = "abcdabcdabcdabcdABCDABCDABCDABCD";
+	const char *expect21 = "abcd";
+	const char *expect22 = "abce";
+	const char *expect23 = "ABCD";
+	const char *expect24 = "ABCE";
+
+	assert(kmp_search(origin2, expect21) == 0);
+	assert(kmp_search(origin2, expect22) == -1);
+	assert(kmp_search(origin2, expect23) == 16);
+	assert(kmp_search(origin2, expect24) == -1);
 
 	return 0;
 }
