bf2a98a0c0a5c77b9561261713c412b6bb72fcf6
Zhou Peng <p@ctriple.cn>
2016-07-11 23:39:51 +0800

    Quicksort test

--- refs/algorithm/sorting/quicksort.c
+++ refs/algorithm/sorting/quicksort.c
@@ -1,20 +1,4 @@
-/* quicksort: sort v[0]..v[n-1] into increasing order */
-void quicksort(int v[], int n)
-{
-	int i, last;
-
-	if (n <= 1) /* nothing to do */
-		return;
-
-	swap(v, 0, rand() % n);  /* move pivot elem to v[0] */
-	last = 0;
-	for (i = 1; i < n; i++)  /* partition */
-		if (v[i] < v[0])
-			swap(v, ++last, i);
-	swap(v, 0, last);        /* restore pivot */
-	quicksort(v, last);      /* recursively sort */
-	quicksort(v+last+1, n-last-1); /*  each part */
-}
+#include <stdlib.h> /* rand() */
 
 /* swap: interchange v[i] and v[j] */
 void swap(int v[], int i, int j)
@@ -24,4 +8,22 @@ void swap(int v[], int i, int j)
 	temp = v[i];
 	v[i] = v[j];
 	v[j] = temp;
+}
+
+/* quicksort: sort v[0]..v[n-1] into increasing order */
+void quicksort(int v[], int n)
+{
+	int i, last;
+
+	if (n <= 1) /* nothing to do */
+		return;
+
+	swap(v, 0, rand()%n); /* move pivot elem to v[0] */
+	last = 0;
+	for (i = 1; i < n; i++)  /* partition */
+		if (v[i] < v[0])
+			swap(v, ++last, i);
+	swap(v, 0, last); /* restore pivot */
+	quicksort(v, last); /* recursively sort */
+	quicksort(v+last+1, n-last-1); /* each part */
 }
