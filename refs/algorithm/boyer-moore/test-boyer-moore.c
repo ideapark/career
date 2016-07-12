0c4d55953cad296085490197cc8b00000911ab6d
Zhou Peng <p@ctriple.cn>
2016-07-12 22:36:02 +0800

    Boyer-Moore search test

---
+++ refs/algorithm/boyer-moore/test-boyer-moore.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <string.h>
+
+int main(void)
+{
+	const char *string1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char *search1 = "xyzABC";
+	const char *search2 = "xyzXYZ";
+
+	char *str1 = boyer_moore(string1, strlen(string1), search1, strlen(search1));
+	assert(str1 != NULL && strncmp(str1, search1, strlen(search1)) == 0);
+
+	char *str2 = boyer_moore(string1, strlen(string1), search2, strlen(search2));
+	assert(str2 == NULL);
+
+	return 0;
+}
