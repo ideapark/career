b3458e21728fe22203e980acd3e5f364e031cef9
Zhou Peng <p@ctriple.cn>
2016-07-14 22:56:49 +0800

    Revised

--- books/practice/test_secret.h
+++ books/practice/test_secret.h
@@ -12,10 +12,10 @@
 /* testmalloc: return NULL after 10 calls */
 void* testmalloc(size_t n)
 {
-    static int count = 0;
+	static int count = 0;
 
-    if (++count > 10)
-        return NULL;
-    else
-        return malloc(n);
+	if (++count > 10)
+		return NULL;
+	else
+		return malloc(n);
 }
