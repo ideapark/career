379c09c72f8943a0694012d5fcf6dd904380d1da
Zhou Peng <p@ctriple.cn>
2016-07-15 20:31:30 +0800

    Revised

--- books/ccore/gcc/hello.c
+++ books/ccore/gcc/hello.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    int i;
-    for (i = 0; i < 10; i++)
-        printf("%s", "Hello, world.\n");
-    return 0;
+	int i;
+	for (i = 0; i < 10; i++)
+		printf("%s", "Hello, world.\n");
+	return 0;
 }
