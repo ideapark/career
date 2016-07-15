c1e7f4c25b43d9acfc734021bda3b1458af71cb1
Zhou Peng <p@ctriple.cn>
2016-07-15 19:37:42 +0800

    KLEE test

--- refs/llvm/klee-maze.c
+++ refs/llvm/klee-maze.c
@@ -6,79 +6,79 @@
 #define W 11
 
 char maze[H][W] = {
-        "+-+---+---+",
-        "| |     |#|",
-        "| | --+ | |",
-        "| |   | | |",
-        "| +-- | | |",
-        "|     |   |",
-        "+-----+---+",
+	"+-+---+---+",
+	"| |     |#|",
+	"| | --+ | |",
+	"| |   | | |",
+	"| +-- | | |",
+	"|     |   |",
+	"+-----+---+",
 };
 
 void draw()
 {
-        int i, j;
-        for (i = 0; i < H; i++) {
-                for (j = 0; j < W; j++)
-                        printf("%c", maze[i][j]);
-                printf("\n");
-        }
-        printf("\n");
+	int i, j;
+	for (i = 0; i < H; i++) {
+		for (j = 0; j < W; j++)
+			printf("%c", maze[i][j]);
+		printf("\n");
+	}
+	printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
-        int x, y; /* player position */
-        int ox, oy; /* old player position */
-        int i = 0; /* iteration number */
+	int x, y; /* player position */
+	int ox, oy; /* old player position */
+	int i = 0; /* iteration number */
 #define ITERS 28
-        char program[ITERS];
+	char program[ITERS];
 
-        x = 1;
-        y = 1;
-        maze[y][x] = 'X';
+	x = 1;
+	y = 1;
+	maze[y][x] = 'X';
 
-        read(0, program, ITERS);
+	read(0, program, ITERS);
 
-        while (i < ITERS) {
-                ox = x;
-                oy = y;
-                switch (program[i]) {
-                case 'w':
-                        y--;
-                        break;
-                case 's':
-                        y++;
-                        break;
-                case 'a':
-                        x--;
-                        break;
-                case 'd':
-                        x++;
-                        break;
-                default:
-                        printf("Wrong command! (only w,s,a,d accepted!)\n");
-                        printf("You lose!\n");
-                        exit(-1);
-                }
-                if (maze[y][x] == '#') {
-                        printf("You win!\n");
-                        printf("Your solution %s\n", program);
-                        exit(1);
-                }
-                if (maze[y][x] != ' ' && !((y == 2 && maze[y][x] == '|' && x > 0 && x < W))) {
-                        x = ox;
-                        y = oy;
-                }
-                if (ox == x && oy == 6) {
-                        printf("You lose\n");
-                        exit(-2);
-                }
-                maze[y][x] = 'X';
-                draw();
-                i++;
-                sleep(1);
-        }
-        printf("You lose\n");
-        return 0;
+	while (i < ITERS) {
+		ox = x;
+		oy = y;
+		switch (program[i]) {
+		case 'w':
+			y--;
+			break;
+		case 's':
+			y++;
+			break;
+		case 'a':
+			x--;
+			break;
+		case 'd':
+			x++;
+			break;
+		default:
+			printf("Wrong command! (only w,s,a,d accepted!)\n");
+			printf("You lose!\n");
+			exit(-1);
+		}
+		if (maze[y][x] == '#') {
+			printf("You win!\n");
+			printf("Your solution %s\n", program);
+			exit(1);
+		}
+		if (maze[y][x] != ' ' && !((y == 2 && maze[y][x] == '|' && x > 0 && x < W))) {
+			x = ox;
+			y = oy;
+		}
+		if (ox == x && oy == 6) {
+			printf("You lose\n");
+			exit(-2);
+		}
+		maze[y][x] = 'X';
+		draw();
+		i++;
+		sleep(1);
+	}
+	printf("You lose\n");
+	return 0;
 }
