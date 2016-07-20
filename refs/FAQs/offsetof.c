f0697b32590863c1c72ea3772af5305b73bfc956
Zhou Peng <p@ctriple.cn>
2016-07-20 21:09:51 +0800

    Revised

--- refs/FAQs/offsetof.c
+++ refs/FAQs/offsetof.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 struct belongings {
-        char   c;
-        short  s;
-        int    i;
-        float  f;
-        double d;
+	char   c;
+	short  s;
+	int    i;
+	float  f;
+	double d;
 };
 
 int main(void)
 {
-        printf("offset c: %lu\n", offsetof(struct belongings, c));
-        printf("offset s: %lu\n", offsetof(struct belongings, s));
-        printf("offset i: %lu\n", offsetof(struct belongings, i));
-        printf("offset d: %lu\n", offsetof(struct belongings, d));
-        return 0;
+	printf("offset c: %lu\n", offsetof(struct belongings, c));
+	printf("offset s: %lu\n", offsetof(struct belongings, s));
+	printf("offset i: %lu\n", offsetof(struct belongings, i));
+	printf("offset d: %lu\n", offsetof(struct belongings, d));
+	return 0;
 }
