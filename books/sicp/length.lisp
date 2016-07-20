1ec1dc26f0f5d9df2900fd40359d8dfa4b9957b7
Zhou Peng <p@ctriple.cn>
2016-07-20 22:56:13 +0800

    list length

---
+++ books/sicp/length.lisp
@@ -0,0 +1,6 @@
+(define (length items)
+        (define (length-iter a count)
+                (if (null? a)
+                    count
+                    (length-iter (cdr a) (+ 1 count))))
+        (length-iter items 0))
