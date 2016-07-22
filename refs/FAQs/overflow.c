9d21425b79abcb0504144835f9320ad009e5886f
Zhou Peng <p@ctriple.cn>
2016-07-22 22:32:30 +0800

    Format

--- refs/FAQs/overflow.c
+++ refs/FAQs/overflow.c
@@ -3,12 +3,12 @@
 
 int foo(int x)
 {
-        return (x+1) > x;
+	return (x+1) > x;
 }
 
 int main(void)
 {
-        printf("%d\n", (INT_MAX+1) > INT_MAX);
-        printf("%d\n", foo(INT_MAX));
-        return 0;
+	printf("%d\n", (INT_MAX+1) > INT_MAX);
+	printf("%d\n", foo(INT_MAX));
+	return 0;
 }
