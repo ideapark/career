04147933d9960bc4cbd8d66b0c9730efd5d78799
Zhou Peng <p@ctriple.cn>
2016-08-07 09:34:23 +0800

    client implementation

--- works/tank/client/client.c
+++ works/tank/client/client.c
@@ -1,6 +1,12 @@
 /*
- * Tank War Client
+ * client.c
  *
  * Copyright (c) Zhou Peng <lockrecv@qq.com>
  */
 
+#include "client.h"
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
--- works/tank/client/client.h
+++ works/tank/client/client.h
@@ -1,5 +1,12 @@
 /*
- * Tank War Client
+ * client.h
  *
  * Copyright (c) Zhou Peng <lockrecv@qq.com>
  */
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+
+
+#endif /* CLIENT_H */
