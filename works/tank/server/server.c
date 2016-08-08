ce86648ea5f491f468fdfdb97dffa3951b93c8d6
Zhou Peng <p@ctriple.cn>
2016-08-08 23:06:22 +0800

    Remove --ip command line argument

--- works/tank/server/server.c
+++ works/tank/server/server.c
@@ -11,18 +11,17 @@
 #include "server.h"
 #include "logger.h"
 #include "map.h"
+#include "link.h"
 
 /*
  * server command line arguments
  */
 static const char *server_port    = "6000";
-static const char *server_ip      = "127.0.0.1";
 static const char *server_map     = "map.txt";
 static const char *server_timeout = "400";
 static const char *server_log     = "server.log";
 
 static struct option server_options[] = {
-	{"ip",      required_argument, 0, 'i'},
 	{"port",    required_argument, 0, 'p'},
 	{"map",     required_argument, 0, 'm'},
 	{"timeout", required_argument, 0, 't'},
@@ -41,11 +40,8 @@ int main(int argc, char *argv[])
 	 * server options
 	 */
 	int c;
-	while ((c = getopt_long(argc, argv, "i:p:m:t:s", server_options, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "p:m:t:s", server_options, NULL)) != -1) {
 		switch (c) {
-		case 'i':
-			server_ip = optarg;
-			break;
 		case 'p':
 			server_port = optarg;
 			break;
