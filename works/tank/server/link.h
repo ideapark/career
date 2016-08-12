b5bd9082ea71001162ea106843a4808502bd08c6
Zhou Peng <p@ctriple.cn>
2016-08-12 12:45:14 +0800

    link comments
    I use link module to care about net connections.

--- works/tank/server/link.h
+++ works/tank/server/link.h
@@ -7,8 +7,19 @@
 #ifndef LINK_H
 #define LINK_H
 
+/*
+ * open listening on port
+ */
 int link_open(const char *port);
+
+/*
+ * stop listening
+ */
 void link_close(void);
+
+/*
+ * accept client connection
+ */
 int link_accept(void);
 
 #endif /* LINK_H */
