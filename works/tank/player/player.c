86ec5edaba76898d426d0358ea3f595df922b0bd
Zhou Peng <p@ctriple.cn>
2016-08-14 00:49:05 +0800

    Player actions level

--- works/tank/player/player.c
+++ works/tank/player/player.c
@@ -12,6 +12,7 @@
 #include "player.h"
 #include "logger.h"
 #include "link.h"
+#include "cJSON.h"
 
 /*
  * client command line arguments
@@ -28,6 +29,31 @@ static struct option client_options[] = {
 };
 
 static int sockfd = -1;
+
+static int do_gamestart(cJSON *body)
+{
+	return 0;
+}
+
+static int do_gameover(cJSON *body)
+{
+	return 0;
+}
+
+static int do_legstart(cJSON *body)
+{
+	return 0;
+}
+
+static int do_legend(cJSON *body)
+{
+	return 0;
+}
+
+static int do_roundstep(cJSON *body)
+{
+	return 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -55,6 +81,37 @@ int main(int argc, char *argv[])
 	/* connect server */
 	if ((sockfd = link_connect(server_ip, server_port)) == -1)
 		exit(-3);
+
+	char buf[BUFFER_MAX+1] = {'\0'};
+	while (read(sockfd, buf, BUFFER_MAX) > 0) {
+		cJSON *root = cJSON_Parse(buf);
+		if (root == NULL) {
+			logger_error("%s\n", "server JSON message invalid.");
+			break;
+		}
+		cJSON *head, *body;
+		if (cJSON_HasObjectItem(root, "head") &&
+		    cJSON_HasObjectItem(root, "body")) {
+			head = cJSON_GetObjectItem(root, "head");
+			body = cJSON_GetObjectItem(root, "body");
+		} else {
+			cJSON_Delete(root);
+			continue;
+		}
+		if (strcmp(head->valuestring, GAME_START) == 0)
+			do_gamestart(body);
+		else if (strcmp(head->valuestring, GAME_OVER) == 0)
+			do_gameover(body);
+		else if (strcmp(head->valuestring, LEG_START) == 0)
+			do_legstart(body);
+		else if (strcmp(head->valuestring, LEG_END) == 0)
+			do_legend(body);
+		else if (strcmp(head->valuestring, ROUND_STEP) == 0)
+			do_roundstep(body);
+		else
+			logger_error("%s\n", "server JSON message head type error.");
+		cJSON_Delete(root);
+	}
 
 	return 0;
 }
--- works/tank/player/player.h
+++ works/tank/player/player.h
@@ -8,5 +8,11 @@
 #define PLAYER_H
 
 #define TEAMNAME "GNU/Linux"
+#define BUFFER_MAX  2048
+#define GAME_START "game start"
+#define GAME_OVER  "game over"
+#define LEG_START  "leg start"
+#define LEG_END    "leg end"
+#define ROUND_STEP "round step"
 
 #endif /* PLAYER_H */
