123045b17491533ee7757d0567da33c3eefe5bf1
Zhou Peng <p@ctriple.cn>
2016-10-08 21:31:08 +0800

    Enumeration

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -120,3 +120,28 @@ typeof flight.constructor // 'function'
 // only object own attributes
 flight.hasOwnProperty('number')       // true
 flight.hasOwnProperty('constructor')  // false
+
+
+/*
+ * Enumeration
+ */
+
+// out of order for-each
+var name;
+for (name in another_stooge) {
+    if (typeof another_stooge[name] !== 'function') {
+	document.writeln(name + ': ' + another_stooge[name]);
+    }
+}
+
+// in order for-each
+var i;
+var properties = [
+    'first-name',
+    'middle-name',
+    'last-name',
+    'profession'
+];
+for (i = 0; i < properties.length; i++) {
+    document.writeln(properties[i] + ': ' + another_stooge[properties[i]]);
+}
