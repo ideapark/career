646cf1fe8ec23153e121f61409d900b9b656ffe8
Zhou Peng <p@ctriple.cn>
2016-10-08 21:35:55 +0800

    Global abatement

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -154,3 +154,30 @@ for (i = 0; i < properties.length; i++) {
 another_stooge.nickname = 'Moe';  // 'Moe'
 delete another_stooge.nickname;   // expose prototype object's property
 another_stooge.nickname           // 'Curly'
+
+
+/*
+ * Global Abatement
+ */
+
+var MYAPP = {};
+
+MYAPP.stooge = {
+    "first-name": "Jerome",
+    "last-name": "Howard"
+};
+
+MYAPP.flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+	IATA: "SYD",
+	time: "2004-09-22 14:55",
+	city: "Sydney"
+    },
+    arrival: {
+	IATA: "LAX",
+	time: "2004-09-23 10:42",
+	city: "Los Angeles"
+    }
+};
