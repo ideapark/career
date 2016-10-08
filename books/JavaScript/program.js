1e1d8580219b706b01791d024735c7d0eaf08841
Zhou Peng <p@ctriple.cn>
2016-10-08 21:01:00 +0800

    Object literal

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -7,3 +7,28 @@ Function.prototype.method = function(name, func) {
 }
 
 document.writeln('Hello, world!');
+
+
+// Object literal
+
+var empty_object = {};
+
+var stooge = {
+    "first-name": "Jerome",
+    "last-name": "Howard"
+};
+
+var flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+	IATA: "SYD",
+	time: "2004-09-22 14:55",
+	city: "Sydney"
+    },
+    arrival: {
+	IATA: "LAX",
+	time: "2004-09-23 10:42",
+	city: "Los Angeles"
+    }
+};
