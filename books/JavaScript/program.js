9c2eb88593eb1e2d23a2df4b7f974fccaa90e592
Zhou Peng <p@ctriple.cn>
2016-10-09 20:18:54 +0800

    Resursion

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -336,3 +336,28 @@ String.method('trim', function(){
     return this.replace(/^\s+|\s+$/g, '');
 });
 document.writeln('"' + "    neat    ".trim() + '"');
+
+
+/*
+ * Recursion
+ */
+
+var hanoi = function(disc, src, aux, dst) {
+    if (disc > 0) {
+	hanoi(disc - 1, src, dst, aux);
+	document.writeln('Move disc ' + disc + ' from ' + src + ' to ' + dst);
+	hanoi(disc - 1, aux, src, dst);
+    }
+};
+
+hanoi(3, 'Src', 'Aux', 'Dst');
+
+var factorial = function factorial(i, a) {
+    a = a || 1;
+    if (i < 2)
+	return a;
+    return factorial(i - 1, a * i);
+};
+
+document.writeln(factorial(4));  // 24
+
