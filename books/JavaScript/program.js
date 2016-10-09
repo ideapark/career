9cdd8b0d7f032de21a315316028a767619e01030
Zhou Peng <p@ctriple.cn>
2016-10-09 19:58:57 +0800

    Exceptions

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -296,3 +296,28 @@ document.writeln(sum(4, 8, 15, 16, 23, 42));  // 108
 
 // return undefined
 // return this
+
+
+/*
+ * Exceptions
+ */
+
+var add = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+	throw {
+	    name: 'TypeError',
+	    message: 'add needs numbers'
+	};
+    }
+    return a + b;
+};
+
+var try_it = function() {
+    try {
+	add("seven");
+    } catch () {
+	document.writeln(e.name + ': ' + e.message);
+    }
+};
+
+try_it();
