581656d2a6eceecbaa63964ac4fa7990c496ecf2
Zhou Peng <p@ctriple.cn>
2016-10-09 19:44:26 +0800

    Function Arguments

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -274,3 +274,18 @@ var statusObject = {
 
 var status = Quo.prototype.get_status.apply(statusObject);
 
+
+/*
+ * Arguments
+ */
+
+var sum = function() {
+    var i, sum = 0;
+    for (i = 0; i < arguments.length; i++) {
+	sum += arguments[i];
+    }
+    return sum;
+};
+
+document.writeln(sum(4, 8, 15, 16, 23, 42));  // 108
+
