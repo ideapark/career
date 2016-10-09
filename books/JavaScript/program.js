eeca3dc366e87b00bf73cfd8f64fc0de72bec4ed
Zhou Peng <p@ctriple.cn>
2016-10-09 19:41:30 +0800

    JavaScript Function Invocation Patterns

--- books/JavaScript/program.js
+++ books/JavaScript/program.js
@@ -202,3 +202,75 @@ var add = function(a, b) {
     return a + b;
 };
 
+
+/*
+ * Invocation
+ *
+ * this
+ * arguments
+ */
+
+
+/*
+ * The Method Invocation Pattern
+ */
+
+var myObject = {
+    value: 0,
+    increment: function(inc) {
+	this.value += typeof inc == 'number' ? inc : 1;
+    }
+};
+
+myObject.increment();
+document.writeln(myObject.value);  // 1
+
+myObject.increment(2);
+document.writeln(myObject.value);  // 3
+
+/*
+ * The Function Invocation Pattern
+ */
+
+var sum = add(3, 4);  // sum's value is 7
+
+myObject.double = function() {
+    var that = this;
+
+    var helper = function() {
+	that.value = add(that.value, that.value);
+    };
+    helper();
+}
+
+myObject.double();
+document.writeln(myObject.value);
+
+/*
+ * The Constructor Invocation Pattern
+ */
+
+var Quo = function(string) {
+    this.status = string;
+};
+
+Quo.prototype.get_status = function() {
+    return this.status;
+};
+
+var myQuo = new Quo("confused");
+document.writeln(myQuo.get_status());  // "confused"
+
+/*
+ * The Apply Invocation Pattern
+ */
+
+var array = [3, 4];
+var sum = add.apply(null, array);  // sum's value is 7
+
+var statusObject = {
+    status: 'A-OK'
+};
+
+var status = Quo.prototype.get_status.apply(statusObject);
+
