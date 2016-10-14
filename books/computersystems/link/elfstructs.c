a2bb57b7a6884324967d0bc901d1b7c4f8fbf1bd
Zhou Peng <p@ctriple.cn>
2016-10-14 20:04:22 +0800

    Elf Symbol

---
+++ books/computersystems/link/elfstructs.c
@@ -0,0 +1,9 @@
+typedef struct {
+	int name;  /* String table offset */
+	int value; /* Section offset, or VM address */
+	int size;  /* Object size in bytes */
+	char type:4,       /* Data, func, section, or src file name (4 bits) */
+		binding:4; /* Local or global (4 bits) */
+	char reserved;     /* Unused */
+	char section;      /* Section header index, ABS, UNDEF, Or COMMON */
+} Elf_Symbol;
