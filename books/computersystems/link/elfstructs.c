3cbda9a03dde2271d2ebc2d99b7832c2343cc5ab
Zhou Peng <p@ctriple.cn>
2016-10-14 22:11:05 +0800

    ELF relocation entry

--- books/computersystems/link/elfstructs.c
+++ books/computersystems/link/elfstructs.c
@@ -7,3 +7,9 @@ typedef struct {
 	char reserved;     /* Unused */
 	char section;      /* Section header index, ABS, UNDEF, Or COMMON */
 } Elf_Symbol;
+
+typedef struct {
+	int offset;     /* Offset of the reference to relocate */
+	int symbol:24,  /* Symbol the reference should point to */
+		type:8; /* Relocation type */
+} Elf32_Rel;
