2a72d3f53ae2a14ffa09d49b1cbf36bb7671f644
Zhou Peng <p@ctriple.cn>
2016-10-14 21:56:47 +0800

    HOWTO static compile process

---
+++ books/computersystems/link/lib/static-lib.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# HOWTO
+#
+# source               main.c   vector.h
+#                        |         |
+#                     +------------------+
+#                     |      compiler    |
+#                     | (cpp, cc1, as)   |
+#                     +------------------+           libvector.a               libc.a      static library
+#                               |                         |                       |
+#  relocatable object file   main.o                       | addvec.o              | printf.o
+#                               |                         |                       |
+#                          +-----------------------------------------------------------+
+#                          |                     linker (ld)                           |
+#                          +-----------------------------------------------------------+
+#                                                      |
+#                                                      p2    static linked executable file
+
+gcc -c addvec.c multvec.c
+ar rcs libvector.a addvec.o multvec.o
+
+gcc -O2 -c main.c
+gcc -static -o p2 main.o ./libvector.a
