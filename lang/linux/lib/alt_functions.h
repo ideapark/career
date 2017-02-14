fe965b4f42bf48209316640b6a605ea5a36ccc7e
Zhou Peng <p@ctriple.cn>
2017-02-14 23:36:07 +0800

    ALT placement functions on other platforms

--- lang/linux/lib/alt_functions.h
+++ lang/linux/lib/alt_functions.h
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU Lesser General Public License as published   *
-* by the Free Software Foundation, either version 3 or (at your option)   *
-* any later version. This program is distributed without any warranty.    *
-* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU Lesser General Public License as published   *
+ * by the Free Software Foundation, either version 3 or (at your option)   *
+ * any later version. This program is distributed without any warranty.    *
+ * See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
 \*************************************************************************/
 
 /* Supplementary program for Chapter 3 */
@@ -17,7 +17,7 @@
 #ifndef ALT_FUNCTIONS_H
 #define ALT_FUNCTIONS_H         /* Prevent accidental double inclusion */
 
-#if defined(__osf__) || defined(__hpux) || defined(_AIX) || \
+#if defined(__osf__) || defined(__hpux) || defined(_AIX) ||	\
         defined(__sgi) || defined(__APPLE__)
 #define strsignal(sig) ALT_strsignal(sig)
 #endif
