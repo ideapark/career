7da5f2254ab321f393b0ea1ce6d50283880a4cf2
Zhou Peng <p@ctriple.cn>
2017-02-14 23:46:17 +0800

    error functions

--- lang/linux/lib/error_functions.h
+++ lang/linux/lib/error_functions.h
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU Lesser General Public License as published   *
-* by the Free Software Foundation, either version 3 or (at your option)   *
-* any later version. This program is distributed without any warranty.    *
-* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU Lesser General Public License as published   *
+ * by the Free Software Foundation, either version 3 or (at your option)   *
+ * any later version. This program is distributed without any warranty.    *
+ * See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
 \*************************************************************************/
 
 /* Listing 3-2 */
@@ -23,25 +23,25 @@ void errMsg(const char *format, ...);
 
 #ifdef __GNUC__
 
-    /* This macro stops 'gcc -Wall' complaining that "control reaches
-       end of non-void function" if we use the following functions to
-       terminate main() or some other non-void function. */
+/* This macro stops 'gcc -Wall' complaining that "control reaches
+   end of non-void function" if we use the following functions to
+   terminate main() or some other non-void function. */
 
 #define NORETURN __attribute__ ((__noreturn__))
 #else
 #define NORETURN
 #endif
 
-void errExit(const char *format, ...) NORETURN ;
+void errExit(const char *format, ...) NORETURN;
 
-void err_exit(const char *format, ...) NORETURN ;
+void err_exit(const char *format, ...) NORETURN;
 
-void errExitEN(int errnum, const char *format, ...) NORETURN ;
+void errExitEN(int errnum, const char *format, ...) NORETURN;
 
-void fatal(const char *format, ...) NORETURN ;
+void fatal(const char *format, ...) NORETURN;
 
-void usageErr(const char *format, ...) NORETURN ;
+void usageErr(const char *format, ...) NORETURN;
 
-void cmdLineErr(const char *format, ...) NORETURN ;
+void cmdLineErr(const char *format, ...) NORETURN;
 
 #endif
