95abdd94a63452d48b36f749c39d0aa026e9826a
Zhou Peng <p@ctriple.cn>
2017-02-15 21:04:13 +0800

    daemon demo app

--- lang/linux/daemons/test_become_daemon.c
+++ lang/linux/daemons/test_become_daemon.c
@@ -1,30 +1,30 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation, either version 3 or (at your option) any      *
+ * later version. This program is distributed without any warranty.  See   *
+ * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
 /* Supplementary program for Chapter 37 */
 
-/* test_become_daemon.c
-
-   Test our becomeDaemon() function.
-*/
+/*
+ * test_become_daemon.c
+ *
+ * Test our becomeDaemon() function.
+ */
 #include "become_daemon.h"
 #include "tlpi_hdr.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    becomeDaemon(0);
+	becomeDaemon(0);
 
-    /* Normally a daemon would live forever; we just sleep for a while */
+	/* Normally a daemon would live forever; we just sleep for a while */
 
-    sleep((argc > 1) ? getInt(argv[1], GN_GT_0, "sleep-time") : 20);
+	sleep((argc > 1) ? getInt(argv[1], GN_GT_0, "sleep-time") : 20);
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
