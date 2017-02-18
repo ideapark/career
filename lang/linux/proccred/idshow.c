1c59160273d50fde01179a2276118c6d20c4985a
Zhou Peng <p@ctriple.cn>
2017-02-18 23:37:01 +0800

    show user ids and groups

--- lang/linux/proccred/idshow.c
+++ lang/linux/proccred/idshow.c
@@ -1,22 +1,13 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 9-1 */
-
-/* idshow.c
-
-   Display all user and group identifiers associated with a process.
-
-   Note: This program uses Linux-specific calls and the Linux-specific
-   file-system user and group IDs.
-*/
+/*
+ * LICENSE: GPL
+ *
+ * idshow.c
+ *
+ * Display all user and group identifiers associated with a process.
+ *
+ * Note: This program uses Linux-specific calls and the Linux-specific
+ * file-system user and group IDs.
+ */
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/fsuid.h>
@@ -26,59 +17,59 @@
 
 #define SG_SIZE (NGROUPS_MAX + 1)
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    uid_t ruid, euid, suid, fsuid;
-    gid_t rgid, egid, sgid, fsgid;
-    gid_t suppGroups[SG_SIZE];
-    int numGroups, j;
-    char *p;
+	uid_t ruid, euid, suid, fsuid;
+	gid_t rgid, egid, sgid, fsgid;
+	gid_t suppGroups[SG_SIZE];
+	int numGroups, j;
+	char *p;
 
-    if (getresuid(&ruid, &euid, &suid) == -1)
-        errExit("getresuid");
-    if (getresgid(&rgid, &egid, &sgid) == -1)
-        errExit("getresgid");
+	if (getresuid(&ruid, &euid, &suid) == -1)
+		errExit("getresuid");
 
-    /* Attempts to change the file-system IDs are always ignored
-       for unprivileged processes, but even so, the following
-       calls return the current file-system IDs */
+	if (getresgid(&rgid, &egid, &sgid) == -1)
+		errExit("getresgid");
 
-    fsuid = setfsuid(0);
-    fsgid = setfsgid(0);
+	/* Attempts to change the file-system IDs are always ignored
+	   for unprivileged processes, but even so, the following
+	   calls return the current file-system IDs */
 
-    printf("UID: ");
-    p = userNameFromId(ruid);
-    printf("real=%s (%ld); ", (p == NULL) ? "???" : p, (long) ruid);
-    p = userNameFromId(euid);
-    printf("eff=%s (%ld); ", (p == NULL) ? "???" : p, (long) euid);
-    p = userNameFromId(suid);
-    printf("saved=%s (%ld); ", (p == NULL) ? "???" : p, (long) suid);
-    p = userNameFromId(fsuid);
-    printf("fs=%s (%ld); ", (p == NULL) ? "???" : p, (long) fsuid);
-    printf("\n");
+	fsuid = setfsuid(0);
+	fsgid = setfsgid(0);
 
-    printf("GID: ");
-    p = groupNameFromId(rgid);
-    printf("real=%s (%ld); ", (p == NULL) ? "???" : p, (long) rgid);
-    p = groupNameFromId(egid);
-    printf("eff=%s (%ld); ", (p == NULL) ? "???" : p, (long) egid);
-    p = groupNameFromId(sgid);
-    printf("saved=%s (%ld); ", (p == NULL) ? "???" : p, (long) sgid);
-    p = groupNameFromId(fsgid);
-    printf("fs=%s (%ld); ", (p == NULL) ? "???" : p, (long) fsgid);
-    printf("\n");
+	printf("UID: ");
+	p = userNameFromId(ruid);
+	printf("real=%s (%ld); ", (p == NULL) ? "???" : p, (long) ruid);
+	p = userNameFromId(euid);
+	printf("eff=%s (%ld); ", (p == NULL) ? "???" : p, (long) euid);
+	p = userNameFromId(suid);
+	printf("saved=%s (%ld); ", (p == NULL) ? "???" : p, (long) suid);
+	p = userNameFromId(fsuid);
+	printf("fs=%s (%ld); ", (p == NULL) ? "???" : p, (long) fsuid);
+	printf("\n");
 
-    numGroups = getgroups(SG_SIZE, suppGroups);
-    if (numGroups == -1)
-        errExit("getgroups");
+	printf("GID: ");
+	p = groupNameFromId(rgid);
+	printf("real=%s (%ld); ", (p == NULL) ? "???" : p, (long) rgid);
+	p = groupNameFromId(egid);
+	printf("eff=%s (%ld); ", (p == NULL) ? "???" : p, (long) egid);
+	p = groupNameFromId(sgid);
+	printf("saved=%s (%ld); ", (p == NULL) ? "???" : p, (long) sgid);
+	p = groupNameFromId(fsgid);
+	printf("fs=%s (%ld); ", (p == NULL) ? "???" : p, (long) fsgid);
+	printf("\n");
 
-    printf("Supplementary groups (%d): ", numGroups);
-    for (j = 0; j < numGroups; j++) {
-        p = groupNameFromId(suppGroups[j]);
-        printf("%s (%ld) ", (p == NULL) ? "???" : p, (long) suppGroups[j]);
-    }
-    printf("\n");
+	numGroups = getgroups(SG_SIZE, suppGroups);
+	if (numGroups == -1)
+		errExit("getgroups");
 
-    exit(EXIT_SUCCESS);
+	printf("Supplementary groups (%d): ", numGroups);
+	for (j = 0; j < numGroups; j++) {
+		p = groupNameFromId(suppGroups[j]);
+		printf("%s (%ld) ", (p == NULL) ? "???" : p, (long) suppGroups[j]);
+	}
+	printf("\n");
+
+	exit(EXIT_SUCCESS);
 }
