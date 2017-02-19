b311715c0bb76348d293794d4a77000cd7357a1c
Zhou Peng <p@ctriple.cn>
2017-02-19 14:59:13 +0800

    license

--- lang/linux/acl/acl_update.c
+++ lang/linux/acl/acl_update.c
@@ -1,16 +1,6 @@
-/*************************************************************************\
- *                  Copyright (C) Michael Kerrisk, 2016.                   *
- *                                                                         *
- * This program is free software. You may use, modify, and redistribute it *
- * under the terms of the GNU General Public License as published by the   *
- * Free Software Foundation, either version 3 or (at your option) any      *
- * later version. This program is distributed without any warranty.  See   *
- * the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Supplementary program for Chapter 17 */
-
 /*
+ * LICENSE: GPL
+ *
  * acl_update.c
  *
  * Perform updates on the access control lists (ACLs) of files named in the
