a5aa5ead31a41516a2fb7b78bdfd25bd8d5e385a
Zhou Peng <p@ctriple.cn>
2017-02-19 14:11:07 +0800

    file input/output

--- lang/linux/daemons/become_daemon.c
+++ lang/linux/daemons/become_daemon.c
@@ -1,16 +1,6 @@
-/*************************************************************************\
- *                  Copyright (C) Michael Kerrisk, 2016.                   *
- *                                                                         *
- * This program is free software. You may use, modify, and redistribute it *
- * under the terms of the GNU Lesser General Public License as published   *
- * by the Free Software Foundation, either version 3 or (at your option)   *
- * any later version. This program is distributed without any warranty.    *
- * See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
-\*************************************************************************/
-
-/* Listing 37-2 */
-
 /*
+ * LICENSE: GPL
+ *
  * become_daemon.c
  *
  *  A function encapsulating the steps in becoming a daemon.
--- lang/linux/daemons/daemon_SIGHUP.c
+++ lang/linux/daemons/daemon_SIGHUP.c
@@ -1,16 +1,6 @@
-/*************************************************************************\
- *                  Copyright (C) Michael Kerrisk, 2016.                   *
- *                                                                         *
- * This program is free software. You may use, modify, and redistribute it *
- * under the terms of the GNU General Public License as published by the   *
- * Free Software Foundation, either version 3 or (at your option) any      *
- * later version. This program is distributed without any warranty.  See   *
- * the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 37-3 */
-
 /*
+ * LICENSE: GPL
+ *
  * daemon_SIGHUP.c
  *
  * Demonstrate the use of SIGHUP as a mechanism to tell a daemon to
