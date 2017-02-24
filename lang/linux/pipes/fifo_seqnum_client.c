dbbc745a78b3b5b12197cdf637286a8cc13a8633
Zhou Peng <p@ctriple.cn>
2017-02-24 17:42:03 +0800

    pipes

--- lang/linux/pipes/fifo_seqnum_client.c
+++ lang/linux/pipes/fifo_seqnum_client.c
@@ -1,83 +1,74 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 44-8 */
-
-/* fifo_seqnum_client.c
-
-   A simple client that uses a well-known FIFO to request a (trivial)
-   "sequence number service". This client creates its own FIFO (using a
-   convention agreed upon by client and server) which is used to receive a reply
-   from the server. The client then sends a request to the server consisting of
-   its PID and the length of the sequence it wishes to be allocated. The client
-   then reads the server's response and displays it on stdout.
-
-   See fifo_seqnum.h for the format of request and response messages.
-
-   The server is in fifo_seqnum_server.c.
-*/
+/*
+ * LICENSE: GPL
+ *
+ * fifo_seqnum_client.c
+ *
+ * A simple client that uses a well-known FIFO to request a (trivial)
+ * "sequence number service". This client creates its own FIFO (using a
+ * convention agreed upon by client and server) which is used to receive a reply
+ * from the server. The client then sends a request to the server consisting of
+ * its PID and the length of the sequence it wishes to be allocated. The client
+ * then reads the server's response and displays it on stdout.
+ *
+ * See fifo_seqnum.h for the format of request and response messages.
+ *
+ * The server is in fifo_seqnum_server.c.
+ */
 #include "fifo_seqnum.h"
 
 static char clientFifo[CLIENT_FIFO_NAME_LEN];
 
-static void             /* Invoked on exit to delete client FIFO */
-removeFifo(void)
+/* Invoked on exit to delete client FIFO */
+static void removeFifo(void)
 {
-    unlink(clientFifo);
+	unlink(clientFifo);
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int serverFd, clientFd;
-    struct request req;
-    struct response resp;
+	int serverFd, clientFd;
+	struct request req;
+	struct response resp;
 
-    if (argc > 1 && strcmp(argv[1], "--help") == 0)
-        usageErr("%s [seq-len...]\n", argv[0]);
+	if (argc > 1 && strcmp(argv[1], "--help") == 0)
+		usageErr("%s [seq-len...]\n", argv[0]);
 
-    /* Create our FIFO (before sending request, to avoid a race) */
+	/* Create our FIFO (before sending request, to avoid a race) */
 
-    umask(0);                   /* So we get the permissions we want */
-    snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE,
-            (long) getpid());
-    if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1
-                && errno != EEXIST)
-        errExit("mkfifo %s", clientFifo);
+	umask(0);                   /* So we get the permissions we want */
+	snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE,
+		 (long) getpid());
 
-    if (atexit(removeFifo) != 0)
-        errExit("atexit");
+	if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1
+	    && errno != EEXIST)
+		errExit("mkfifo %s", clientFifo);
 
-    /* Construct request message, open server FIFO, and send message */
+	if (atexit(removeFifo) != 0)
+		errExit("atexit");
 
-    req.pid = getpid();
-    req.seqLen = (argc > 1) ? getInt(argv[1], GN_GT_0, "seq-len") : 1;
+	/* Construct request message, open server FIFO, and send message */
 
-    serverFd = open(SERVER_FIFO, O_WRONLY);
-    if (serverFd == -1)
-        errExit("open %s", SERVER_FIFO);
+	req.pid = getpid();
+	req.seqLen = (argc > 1) ? getInt(argv[1], GN_GT_0, "seq-len") : 1;
 
-    if (write(serverFd, &req, sizeof(struct request)) !=
+	serverFd = open(SERVER_FIFO, O_WRONLY);
+	if (serverFd == -1)
+		errExit("open %s", SERVER_FIFO);
+
+	if (write(serverFd, &req, sizeof(struct request)) !=
             sizeof(struct request))
-        fatal("Can't write to server");
+		fatal("Can't write to server");
 
-    /* Open our FIFO, read and display response */
+	/* Open our FIFO, read and display response */
 
-    clientFd = open(clientFifo, O_RDONLY);
-    if (clientFd == -1)
-        errExit("open %s", clientFifo);
+	clientFd = open(clientFifo, O_RDONLY);
+	if (clientFd == -1)
+		errExit("open %s", clientFifo);
 
-    if (read(clientFd, &resp, sizeof(struct response))
+	if (read(clientFd, &resp, sizeof(struct response))
             != sizeof(struct response))
-        fatal("Can't read response from server");
+		fatal("Can't read response from server");
 
-    printf("%d\n", resp.seqNum);
-    exit(EXIT_SUCCESS);
+	printf("%d\n", resp.seqNum);
+	exit(EXIT_SUCCESS);
 }
