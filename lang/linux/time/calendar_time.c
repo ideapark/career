9e9d7a0e12fd271cc6348c376dd4f8369f43d053
Zhou Peng <p@ctriple.cn>
2017-02-25 23:03:04 +0800

    license

--- lang/linux/time/calendar_time.c
+++ lang/linux/time/calendar_time.c
@@ -1,16 +1,6 @@
-/*************************************************************************\
- *                  Copyright (C) Michael Kerrisk, 2016.                   *
- *                                                                         *
- * This program is free software. You may use, modify, and redistribute it *
- * under the terms of the GNU General Public License as published by the   *
- * Free Software Foundation, either version 3 or (at your option) any      *
- * later version. This program is distributed without any warranty.  See   *
- * the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 10-1 */
-
 /*
+ * LICENSE: GPL
+ *
  * calendar_time.c
  *
  * Demonstrate the use of functions for working with calendar time.
--- lang/linux/time/show_time.c
+++ lang/linux/time/show_time.c
@@ -1,16 +1,6 @@
-/*************************************************************************\
- *                  Copyright (C) Michael Kerrisk, 2016.                   *
- *                                                                         *
- * This program is free software. You may use, modify, and redistribute it *
- * under the terms of the GNU General Public License as published by the   *
- * Free Software Foundation, either version 3 or (at your option) any      *
- * later version. This program is distributed without any warranty.  See   *
- * the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 10-4 */
-
 /*
+ * LICENSE: GPL
+ *
  * show_time.c
  *
  * A short program that allows us to see the effects of locale and timezone
