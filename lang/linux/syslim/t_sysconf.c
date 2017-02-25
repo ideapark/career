c028de3865812d3d7665a1b43fb250533add5792
Zhou Peng <p@ctriple.cn>
2017-02-25 22:59:30 +0800

    Review

--- lang/linux/syslim/t_sysconf.c
+++ lang/linux/syslim/t_sysconf.c
@@ -1,16 +1,6 @@
-/*************************************************************************\
- *                  Copyright (C) Michael Kerrisk, 2016.                   *
- *                                                                         *
- * This program is free software. You may use, modify, and redistribute it *
- * under the terms of the GNU General Public License as published by the   *
- * Free Software Foundation, either version 3 or (at your option) any      *
- * later version. This program is distributed without any warranty.  See   *
- * the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 11-1 */
-
 /*
+ * LICENSE: GPL
+ *
  * t_sysconf.c
  *
  * Demonstrate the use of sysconf() to retrieve system limits.
