b066a552514ee4efde8800d559cbffd053ad7505
Zhou Peng <p@ctriple.cn>
2017-02-28 23:09:04 +0800

    longjmp & environ

--- lang/linux/proc/setenv.c
+++ lang/linux/proc/setenv.c
@@ -1,112 +1,100 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Solution for Exercise 6-3 */
-
-/* setenv.c
-
-   An implementation of setenv() and unsetenv() using environ, putenv(),
-   and getenv().
-*/
+/*
+ * LICENSE: GPL
+ *
+ * setenv.c
+ *
+ * An implementation of setenv() and unsetenv() using environ, putenv(),
+ * and getenv().
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 
-int
-unsetenv(const char *name)
+int unsetenv(const char *name)
 {
-    extern char **environ;
-    char **ep, **sp;
-    size_t len;
+	extern char **environ;
+	char **ep, **sp;
+	size_t len;
 
-    if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL) {
-        errno = EINVAL;
-        return -1;
-    }
+	if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL) {
+		errno = EINVAL;
+		return -1;
+	}
 
-    len = strlen(name);
+	len = strlen(name);
 
-    for (ep = environ; *ep != NULL; )
-        if (strncmp(*ep, name, len) == 0 && (*ep)[len] == '=') {
+	for (ep = environ; *ep != NULL; )
+		if (strncmp(*ep, name, len) == 0 && (*ep)[len] == '=') {
 
-            /* Remove found entry by shifting all successive entries
-               back one element */
+			/* Remove found entry by shifting all successive entries
+			   back one element */
 
-            for (sp = ep; *sp != NULL; sp++)
-                *sp = *(sp + 1);
+			for (sp = ep; *sp != NULL; sp++)
+				*sp = *(sp + 1);
 
-            /* Continue around the loop to further instances of 'name' */
+			/* Continue around the loop to further instances of 'name' */
 
-        } else {
-            ep++;
-        }
+		} else {
+			ep++;
+		}
 
-    return 0;
+	return 0;
 }
 
-int
-setenv(const char *name, const char *value, int overwrite)
+int setenv(const char *name, const char *value, int overwrite)
 {
-    char *es;
+	char *es;
 
-    if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL ||
+	if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL ||
             value == NULL) {
-        errno = EINVAL;
-        return -1;
-    }
+		errno = EINVAL;
+		return -1;
+	}
 
-    if (getenv(name) != NULL && overwrite == 0)
-        return 0;
+	if (getenv(name) != NULL && overwrite == 0)
+		return 0;
 
-    unsetenv(name);             /* Remove all occurrences */
+	unsetenv(name);             /* Remove all occurrences */
 
-    es = malloc(strlen(name) + strlen(value) + 2);
-                                /* +2 for '=' and null terminator */
-    if (es == NULL)
-        return -1;
+	es = malloc(strlen(name) + strlen(value) + 2);
+	/* +2 for '=' and null terminator */
+	if (es == NULL)
+		return -1;
 
-    strcpy(es, name);
-    strcat(es, "=");
-    strcat(es, value);
+	strcpy(es, name);
+	strcat(es, "=");
+	strcat(es, value);
 
-    return (putenv(es) != 0) ? -1 : 0;
+	return (putenv(es) != 0) ? -1 : 0;
 }
 
 #ifdef TEST_IT
 
-int
-main()
+int main()
 {
-    if (putenv("TT=xxxxx") != 0)
-        perror("putenv");
+	if (putenv("TT=xxxxx") != 0)
+		perror("putenv");
 
-    system("echo '***** Environment before unsetenv(TT)'; "
-            "printenv | grep ^TT");
-    system("echo 'Total lines from printenv:' `printenv | wc -l`");
+	system("echo '***** Environment before unsetenv(TT)'; "
+	       "printenv | grep ^TT");
+	system("echo 'Total lines from printenv:' `printenv | wc -l`");
 
-    unsetenv("TT");
+	unsetenv("TT");
 
-    system("echo '***** Environment after unsetenv(TT)'; "
-            "printenv | grep ^TT");
-    system("echo 'Total lines from printenv:' `printenv | wc -l`");
+	system("echo '***** Environment after unsetenv(TT)'; "
+	       "printenv | grep ^TT");
+	system("echo 'Total lines from printenv:' `printenv | wc -l`");
 
-    setenv("xyz", "one", 1);
-    setenv("xyz", "two", 0);
-    setenv("xyz2", "222", 0);
+	setenv("xyz", "one", 1);
+	setenv("xyz", "two", 0);
+	setenv("xyz2", "222", 0);
 
-    system("echo '***** Environment after setenv() calls'; "
-            "printenv | grep ^x");
-    system("echo 'Total lines from printenv:' `printenv | wc -l`");
+	system("echo '***** Environment after setenv() calls'; "
+	       "printenv | grep ^x");
+	system("echo 'Total lines from printenv:' `printenv | wc -l`");
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
 #endif
--- lang/linux/proc/t_getenv.c
+++ lang/linux/proc/t_getenv.c
@@ -1,32 +1,22 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Supplementary program for Chapter 6 */
-
-/* t_getenv.c
-
-   Demonstrate the use of getenv() to retrieve the value of an
-   environment variable.
-*/
+/*
+ * LICENSE: GPL
+ *
+ * t_getenv.c
+ *
+ * Demonstrate the use of getenv() to retrieve the value of an
+ * environment variable.
+ */
 #include "tlpi_hdr.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    char *val;
+	char *val;
 
-    if (argc != 2 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s environ-var\n", argv[0]);
+	if (argc != 2 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s environ-var\n", argv[0]);
 
-    val = getenv(argv[1]);
-    printf("%s\n", (val != NULL) ? val : "No such variable");
+	val = getenv(argv[1]);
+	printf("%s\n", (val != NULL) ? val : "No such variable");
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
