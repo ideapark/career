29be3be35414b6ad7422834a1b8ef94608d7072b
Zhou Peng <p@ctriple.cn>
2017-03-07 22:26:14 +0800

    linux memory allocator
    delayed paging
    mmap() area
    anonymous mapping

--- refs/FAQs/oom/loop-calloc.c
+++ refs/FAQs/oom/loop-calloc.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEGABYTE  (1024 * 1024)
+#define MEGABYTE  (1024*1024)
 
 int main(int argc, char *argv[])
 {
 	void *block = NULL;
 	int count = 0;
-	
+
 	while (1) {
 		block = (void *)malloc(MEGABYTE);
 		if (!block)
