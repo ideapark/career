ea383cbcebe2de01725b38200949d300d704b8e6
Zhou Peng <p@ctriple.cn>
2017-03-07 20:03:56 +0800

    Create loop-calloc.c

---
+++ refs/FAQs/oom/loop-calloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MEGABYTE  (1024 * 1024)
+
+int main(int argc, char *argv[])
+{
+	void *block = NULL;
+	int count = 0;
+	
+	while (1) {
+		block = (void *)malloc(MEGABYTE);
+		if (!block)
+			break;
+		memset(block, 1, MEGABYTE);
+		printf("Currently allocating %d MB\n", ++count);
+	}
+	getchar();
+	return 0;
+}
