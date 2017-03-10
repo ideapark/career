e9ec1ed4a98038c3ca8a6aac40f3016aace41a66
Zhou Peng <p@ctriple.cn>
2017-03-11 00:29:05 +0800

    linux signal handling

--- lang/linux/signals/sig_receiver.c
+++ lang/linux/signals/sig_receiver.c
@@ -1,24 +1,15 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 20-7 */
-
-/* sig_receiver.c
-
-   Usage: sig_receiver [block-time]
-
-   Catch and report statistics on signals sent by sig_sender.c.
-
-   Note that although we use signal() to establish the signal handler in this
-   program, the use of sigaction() is always preferable for this task.
-*/
+/*
+ * LICENSE: GPL
+ *
+ * sig_receiver.c
+ *
+ * Usage: sig_receiver [block-time]
+ *
+ * Catch and report statistics on signals sent by sig_sender.c.
+ *
+ * Note that although we use signal() to establish the signal handler in this
+ * program, the use of sigaction() is always preferable for this task.
+ */
 #define _GNU_SOURCE
 #include <signal.h>
 #include "signal_functions.h"           /* Declaration of printSigset() */
@@ -26,64 +17,62 @@
 
 static int sigCnt[NSIG];                /* Counts deliveries of each signal */
 static volatile sig_atomic_t gotSigint = 0;
-                                        /* Set nonzero if SIGINT is delivered */
+/* Set nonzero if SIGINT is delivered */
 
-static void
-handler(int sig)
+static void handler(int sig)
 {
-    if (sig == SIGINT)
-        gotSigint = 1;
-    else
-        sigCnt[sig]++;
+	if (sig == SIGINT)
+		gotSigint = 1;
+	else
+		sigCnt[sig]++;
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int n, numSecs;
-    sigset_t pendingMask, blockingMask, emptyMask;
+	int n, numSecs;
+	sigset_t pendingMask, blockingMask, emptyMask;
 
-    printf("%s: PID is %ld\n", argv[0], (long) getpid());
+	printf("%s: PID is %ld\n", argv[0], (long) getpid());
 
-    /* Here we use the simpler signal() API to establish a signal handler,
-       but for the reasons described in Section 22.7 of TLPI, sigaction()
-       is the (strongly) preferred API for this task. */
+	/* Here we use the simpler signal() API to establish a signal handler,
+	   but for the reasons described in Section 22.7 of TLPI, sigaction()
+	   is the (strongly) preferred API for this task. */
 
-    for (n = 1; n < NSIG; n++)          /* Same handler for all signals */
-        (void) signal(n, handler);      /* Ignore errors */
+	for (n = 1; n < NSIG; n++)          /* Same handler for all signals */
+		(void) signal(n, handler);      /* Ignore errors */
 
-    /* If a sleep time was specified, temporarily block all signals,
-       sleep (while another process sends us signals), and then
-       display the mask of pending signals and unblock all signals */
+	/* If a sleep time was specified, temporarily block all signals,
+	   sleep (while another process sends us signals), and then
+	   display the mask of pending signals and unblock all signals */
 
-    if (argc > 1) {
-        numSecs = getInt(argv[1], GN_GT_0, NULL);
+	if (argc > 1) {
+		numSecs = getInt(argv[1], GN_GT_0, NULL);
 
-        sigfillset(&blockingMask);
-        if (sigprocmask(SIG_SETMASK, &blockingMask, NULL) == -1)
-            errExit("sigprocmask");
+		sigfillset(&blockingMask);
+		if (sigprocmask(SIG_SETMASK, &blockingMask, NULL) == -1)
+			errExit("sigprocmask");
 
-        printf("%s: sleeping for %d seconds\n", argv[0], numSecs);
-        sleep(numSecs);
+		printf("%s: sleeping for %d seconds\n", argv[0], numSecs);
+		sleep(numSecs);
 
-        if (sigpending(&pendingMask) == -1)
-            errExit("sigpending");
+		if (sigpending(&pendingMask) == -1)
+			errExit("sigpending");
 
-        printf("%s: pending signals are: \n", argv[0]);
-        printSigset(stdout, "\t\t", &pendingMask);
+		printf("%s: pending signals are: \n", argv[0]);
+		printSigset(stdout, "\t\t", &pendingMask);
 
-        sigemptyset(&emptyMask);        /* Unblock all signals */
-        if (sigprocmask(SIG_SETMASK, &emptyMask, NULL) == -1)
-            errExit("sigprocmask");
-    }
+		sigemptyset(&emptyMask);        /* Unblock all signals */
+		if (sigprocmask(SIG_SETMASK, &emptyMask, NULL) == -1)
+			errExit("sigprocmask");
+	}
 
-    while (!gotSigint)                  /* Loop until SIGINT caught */
-        continue;
+	while (!gotSigint)                  /* Loop until SIGINT caught */
+		continue;
 
-    for (n = 1; n < NSIG; n++)          /* Display number of signals received */
-        if (sigCnt[n] != 0)
-            printf("%s: signal %d caught %d time%s\n", argv[0], n,
-                    sigCnt[n], (sigCnt[n] == 1) ? "" : "s");
+	for (n = 1; n < NSIG; n++)          /* Display number of signals received */
+		if (sigCnt[n] != 0)
+			printf("%s: signal %d caught %d time%s\n", argv[0], n,
+			       sigCnt[n], (sigCnt[n] == 1) ? "" : "s");
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
--- lang/linux/signals/sigmask_longjmp.c
+++ lang/linux/signals/sigmask_longjmp.c
@@ -1,23 +1,14 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 21-2 */
-
-/* sigmask_longjmp.c
-
-   Demonstrate the different effects of longjmp() and siglongjmp()
-   on the process signal mask.
-
-   By default, this program uses setjmp() + longjmp(). Compile with
-   -DUSE_SIGSETJMP to use sigsetjmp() + siglongjmp().
-*/
+/*
+ * LICENSE: GPL
+ *
+ * sigmask_longjmp.c
+ *
+ * Demonstrate the different effects of longjmp() and siglongjmp()
+ * on the process signal mask.
+ *
+ * By default, this program uses setjmp() + longjmp(). Compile with
+ * -DUSE_SIGSETJMP to use sigsetjmp() + siglongjmp().
+ */
 #define _GNU_SOURCE     /* Get strsignal() declaration from <string.h> */
 #include <string.h>
 #include <setjmp.h>
@@ -26,61 +17,59 @@
 #include "tlpi_hdr.h"
 
 static volatile sig_atomic_t canJump = 0;
-                        /* Set to 1 once "env" buffer has been
-                           initialized by [sig]setjmp() */
+/* Set to 1 once "env" buffer has been
+   initialized by [sig]setjmp() */
 #ifdef USE_SIGSETJMP
 static sigjmp_buf senv;
 #else
 static jmp_buf env;
 #endif
 
-static void
-handler(int sig)
+static void handler(int sig)
 {
-    /* UNSAFE: This handler uses non-async-signal-safe functions
-       (printf(), strsignal(), printSigMask(); see Section 21.1.2) */
+	/* UNSAFE: This handler uses non-async-signal-safe functions
+	   (printf(), strsignal(), printSigMask(); see Section 21.1.2) */
 
-    printf("Received signal %d (%s), signal mask is:\n", sig,
-            strsignal(sig));
-    printSigMask(stdout, NULL);
+	printf("Received signal %d (%s), signal mask is:\n", sig,
+	       strsignal(sig));
+	printSigMask(stdout, NULL);
 
-    if (!canJump) {
-        printf("'env' buffer not yet set, doing a simple return\n");
-        return;
-    }
+	if (!canJump) {
+		printf("'env' buffer not yet set, doing a simple return\n");
+		return;
+	}
 
 #ifdef USE_SIGSETJMP
-    siglongjmp(senv, 1);
+	siglongjmp(senv, 1);
 #else
-    longjmp(env, 1);
+	longjmp(env, 1);
 #endif
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    struct sigaction sa;
+	struct sigaction sa;
 
-    printSigMask(stdout, "Signal mask at startup:\n");
+	printSigMask(stdout, "Signal mask at startup:\n");
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = handler;
-    if (sigaction(SIGINT, &sa, NULL) == -1)
-        errExit("sigaction");
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		errExit("sigaction");
 
 #ifdef USE_SIGSETJMP
-    printf("Calling sigsetjmp()\n");
-    if (sigsetjmp(senv, 1) == 0)
+	printf("Calling sigsetjmp()\n");
+	if (sigsetjmp(senv, 1) == 0)
 #else
-    printf("Calling setjmp()\n");
-    if (setjmp(env) == 0)
+		printf("Calling setjmp()\n");
+	if (setjmp(env) == 0)
 #endif
-        canJump = 1;                    /* Executed after [sig]setjmp() */
+		canJump = 1;                    /* Executed after [sig]setjmp() */
 
-    else                                /* Executed after [sig]longjmp() */
-        printSigMask(stdout, "After jump from handler, signal mask is:\n" );
+	else                                /* Executed after [sig]longjmp() */
+		printSigMask(stdout, "After jump from handler, signal mask is:\n" );
 
-    for (;;)                            /* Wait for signals until killed */
-        pause();
+	for (;;)                            /* Wait for signals until killed */
+		pause();
 }
