8c7cb34f3c7e64e5b9f8bce1384a5223d57e8dd3
Zhou Peng <p@ctriple.cn>
2017-03-16 22:29:49 +0800

    go doc

---
+++ lang/golang/godoc.go
@@ -0,0 +1,12 @@
+// Package superman implements methods for saving the world.
+//
+// Experience has shown that a small number of procedures can
+// prove helpful when attempting to save the world.
+package superman
+
+// enterOrbit causes Superman to fly into low Earth orbit, a
+// position that presents several possibilities for planet
+// salvation.
+func enterOrbit() error {
+	// ...
+}
