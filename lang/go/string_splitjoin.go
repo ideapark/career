3a02438a26867d65c576e98bea43c59b823539f3
Zhou Peng <p@ctriple.cn>
2017-03-18 20:54:15 +0800

    go string split/join

---
+++ lang/go/string_splitjoin.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "The quick brown fox jumps over the lazy dog"
+	s1 := strings.Fields(str)
+	fmt.Printf("Splitted in slice: %v\n", s1)
+	for _, val := range s1 {
+		fmt.Printf("%s - ", val)
+	}
+	fmt.Println()
+	str2 := "GO1|The ABC of GO|25"
+	s12 := strings.Split(str2, "|")
+	fmt.Printf("Splitted in slice: %v\n", s12)
+	for _, val := range s12 {
+		fmt.Printf("%s - ", val)
+	}
+	fmt.Println()
+	str3 := strings.Join(s12, ";")
+	fmt.Printf("s12 joined by ;: %s\n", str3)
+}
