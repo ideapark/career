f45b702ae77e3890e68ba8910327b0b669f9f0ca
Zhou Peng <p@ctriple.cn>
2017-03-29 22:51:05 +0800

    golang standard package 'archive/zip'

---
+++ lang/go/standard-library/archive/zip/regcomp.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"compress/flate"
+	"io"
+)
+
+func main() {
+	// Override the default Deflate compressor with a higher compression level.
+
+	// Create a buffer to write our archive to.
+	buf := new(bytes.Buffer)
+
+	// Create a new zip archive.
+	w := zip.NewWriter(buf)
+
+	// Register a custom Deflate compressor.
+	w.RegisterCompressor(zip.Deflate, func(out io.Writer) (io.WriteCloser, error) {
+		return flate.NewWriter(out, flate.BestCompression)
+	})
+
+	// Proceed to add files to w.
+}
