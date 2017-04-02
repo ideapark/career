f07147ce76fbca96fe37284a69c469ab80eeb731
Zhou Peng <p@ctriple.cn>
2017-04-02 10:56:54 +0800

    bytes.Map

---
+++ lang/go/standard-library/bytes/bytes_map.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	rot13 := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'z':
+			return 'A' + (r-'A'+13)%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'+13)%26
+		}
+		return r
+	}
+	fmt.Printf("%s", bytes.Map(rot13, []byte("Twas brilling and the slithy gopher...")))
+}
