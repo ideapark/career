3a9354e84df9f8353da0d368c90ba8d977ca0ddc
Zhou Peng <p@ctriple.cn>
2017-04-02 10:30:53 +0800

    bytes.HasSuffix

---
+++ lang/go/standard-library/bytes/bytes_hassuffix.go
@@ -0,0 +1,14 @@
+// HasSuffix tests whether the byte slice s ends with suffix.
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(bytes.HasSuffix([]byte("Amigo"), []byte("go")))
+	fmt.Println(bytes.HasSuffix([]byte("Amigo"), []byte("O")))
+	fmt.Println(bytes.HasSuffix([]byte("Amigo"), []byte("Ami")))
+	fmt.Println(bytes.HasSuffix([]byte("Amigo"), []byte("")))
+}
