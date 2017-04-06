03589162a7923865348cf14163ec1a1cfeb0848c
Zhou Peng <p@ctriple.cn>
2017-04-06 19:26:17 +0800

    Create base32_decodestring.go

---
+++ lang/go/standard-library/encoding/base32/base32_decodestring.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/base32"
+	"fmt"
+)
+
+func main() {
+	str := "ONXW2ZJAMRQXIYJAO5UXI2BAAAQGC3TEEDX3XPY="
+	data, err := base32.StdEncoding.DecodeString(str)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%q\n", data)
+}
