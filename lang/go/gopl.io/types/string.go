620cb0d2bcb420ee9766852498df5b1459c5f409
Zhou Peng <p@ctriple.cn>
2017-04-16 22:59:08 +0800

    golang string handling

---
+++ lang/go/gopl.io/types/string.go
@@ -0,0 +1,24 @@
+package string
+
+import (
+	"strings"
+)
+
+// basename removes directory components and a .suffix.
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3] + "," + s[n-3:])
+}
