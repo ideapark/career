ce04f5b3ef8248090f32f251bacfe14fb1c1bb78
Zhou Peng <p@ctriple.cn>
2017-04-21 22:33:54 +0800

    defer print stack trace

--- lang/go/gopl.io/comp/panic.go
+++ lang/go/gopl.io/comp/panic.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
+	defer printStack()
 	f(3)
 }
 
@@ -10,4 +15,10 @@ func f(x int) {
 	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
 	defer fmt.Printf("defer %d\n", x)
 	f(x - 1)
+}
+
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
 }
