1ab67715214a7697689ef498e1e2f0abaa748f05
Zhou Peng <p@ctriple.cn>
2017-05-01 11:18:06 +0800

    OAuth2

--- lang/go/blueprints/chat/main.go
+++ lang/go/blueprints/chat/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/stretchr/gomniauth/provider/facebook"
+	"github.com/stretchr/gomniauth/provider/github"
+	"github.com/stretchr/gomniauth/provider/google"
+	"github.com/stretchr/objx"
 	"log"
 	"net/http"
 	"os"
@@ -22,16 +26,32 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse() // parse the flags
 
+	// set up gomniauth
+	gomniauth.SetSecurityKey("some long key")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/callback/github"),
+		google.New("key", "secret", "http://localhost:8080/callback/google"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// get the room going
