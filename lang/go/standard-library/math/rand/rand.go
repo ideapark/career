b375f97547849bcf1cb307cd1223c56bff8da8db
Zhou Peng <p@ctriple.cn>
2017-05-05 22:10:23 +0800

    package math

---
+++ lang/go/standard-library/math/rand/rand.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	rand.Seed(42) // Try changing this number!
+	answers := []string{
+		"It is certain",
+		"it is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
+	}
+	fmt.Println("Magic 8-Ball says:", answers[rand.Intn(len(answers))])
+}
