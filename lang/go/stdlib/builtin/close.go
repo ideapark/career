fed74612a50c411341bf6597bce37963588b5449
Zhou Peng <p@ctriple.cn>
2017-05-19 14:14:00 +0800

    golang builitin function close()

---
+++ lang/go/stdlib/builtin/close.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	c := make(chan int, 1)
+	c <- 1
+
+	log.Println(<-c) // Prints 1
+
+	c <- 2
+	close(c)
+
+	log.Println(<-c) // Prints 2
+	log.Println(<-c) // Prints 0
+
+	if i, ok := <-c; ok {
+		log.Printf("Channel is open, go %d", i)
+	} else {
+		log.Printf("Channel is closed, got %d", i)
+	}
+
+	close(c) // Panics, channel is already closed
+}
