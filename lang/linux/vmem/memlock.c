61a5ae3c0673dbe54dc8335038928be957d2772f
Zhou Peng <p@ctriple.cn>
2017-07-05 12:28:39 +0800

    Linux virtual memory management

--- lang/linux/vmem/memlock.c
+++ lang/linux/vmem/memlock.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation, either version 3 or (at your option) any      *
+ * later version. This program is distributed without any warranty.  See   *
+ * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
 /* Listing 50-2 */
@@ -27,78 +27,76 @@
 
 /* Display residency of pages in range [addr .. (addr + length - 1)] */
 
-static void
-displayMincore(char *addr, size_t length)
+static void displayMincore(char *addr, size_t length)
 {
-    unsigned char *vec;
-    long pageSize, numPages, j;
+	unsigned char *vec;
+	long pageSize, numPages, j;
 
 #ifndef _SC_PAGESIZE
-    pageSize = getpagesize();   /* Some systems don't have _SC_PAGESIZE */
+	pageSize = getpagesize();   /* Some systems don't have _SC_PAGESIZE */
 #else
-    pageSize = sysconf(_SC_PAGESIZE);
+	pageSize = sysconf(_SC_PAGESIZE);
 #endif
 
-    numPages = (length + pageSize - 1) / pageSize;
-    vec = malloc(numPages);
-    if (vec == NULL)
-        errExit("malloc");
+	numPages = (length + pageSize - 1) / pageSize;
+	vec = malloc(numPages);
+	if (vec == NULL)
+		errExit("malloc");
 
-    if (mincore(addr, length, vec) == -1)
-        errExit("mincore");
+	if (mincore(addr, length, vec) == -1)
+		errExit("mincore");
 
-    for (j = 0; j < numPages; j++) {
-        if (j % 64 == 0)
-            printf("%s%10p: ", (j == 0) ? "" : "\n", addr + (j * pageSize));
-        printf("%c", (vec[j] & 1) ? '*' : '.');
-    }
-    printf("\n");
+	for (j = 0; j < numPages; j++) {
+		if (j % 64 == 0)
+			printf("%s%10p: ", (j == 0) ? "" : "\n", addr + (j * pageSize));
+		printf("%c", (vec[j] & 1) ? '*' : '.');
+	}
+	printf("\n");
 
-    free(vec);
+	free(vec);
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    char *addr;
-    size_t len, lockLen;
-    long pageSize, stepSize, j;
+	char *addr;
+	size_t len, lockLen;
+	long pageSize, stepSize, j;
 
-    if (argc != 4 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s num-pages lock-page-step lock-page-len\n", argv[0]);
+	if (argc != 4 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s num-pages lock-page-step lock-page-len\n", argv[0]);
 
 #ifndef _SC_PAGESIZE
-    pageSize = getpagesize();
-    if (pageSize == -1)
-        errExit("getpagesize");
+	pageSize = getpagesize();
+	if (pageSize == -1)
+		errExit("getpagesize");
 #else
-    pageSize = sysconf(_SC_PAGESIZE);
-    if (pageSize == -1)
-        errExit("sysconf(_SC_PAGESIZE)");
+	pageSize = sysconf(_SC_PAGESIZE);
+	if (pageSize == -1)
+		errExit("sysconf(_SC_PAGESIZE)");
 #endif
 
-    len =      getInt(argv[1], GN_GT_0, "num-pages") * pageSize;
-    stepSize = getInt(argv[2], GN_GT_0, "lock-page-step") * pageSize;
-    lockLen =  getInt(argv[3], GN_GT_0, "lock-page-len") * pageSize;
+	len =      getInt(argv[1], GN_GT_0, "num-pages") * pageSize;
+	stepSize = getInt(argv[2], GN_GT_0, "lock-page-step") * pageSize;
+	lockLen =  getInt(argv[3], GN_GT_0, "lock-page-len") * pageSize;
 
-    addr = mmap(NULL, len, PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if (addr == MAP_FAILED)
-        errExit("mmap");
+	addr = mmap(NULL, len, PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	if (addr == MAP_FAILED)
+		errExit("mmap");
 
-    printf("Allocated %ld (%#lx) bytes starting at %p\n",
-            (long) len, (unsigned long) len, addr);
+	printf("Allocated %ld (%#lx) bytes starting at %p\n",
+	       (long) len, (unsigned long) len, addr);
 
-    printf("Before mlock:\n");
-    displayMincore(addr, len);
+	printf("Before mlock:\n");
+	displayMincore(addr, len);
 
-    /* Lock pages specified by command-line arguments into memory */
+	/* Lock pages specified by command-line arguments into memory */
 
-    for (j = 0; j + lockLen <= len; j += stepSize)
-        if (mlock(addr + j, lockLen) == -1)
-            errExit("mlock");
+	for (j = 0; j + lockLen <= len; j += stepSize)
+		if (mlock(addr + j, lockLen) == -1)
+			errExit("mlock");
 
-    printf("After mlock:\n");
-    displayMincore(addr, len);
+	printf("After mlock:\n");
+	displayMincore(addr, len);
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
--- lang/linux/vmem/t_mprotect.c
+++ lang/linux/vmem/t_mprotect.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation, either version 3 or (at your option) any      *
+ * later version. This program is distributed without any warranty.  See   *
+ * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
 /* Listing 50-1 */
@@ -25,33 +25,32 @@
 
 #define SHELL_FMT "cat /proc/%ld/maps | grep zero"
 #define CMD_SIZE (sizeof(SHELL_FMT) + 20)
-                            /* Allow extra space for integer string */
+/* Allow extra space for integer string */
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    char cmd[CMD_SIZE];
-    char *addr;
+	char cmd[CMD_SIZE];
+	char *addr;
 
-    /* Create an anonymous mapping with all access denied */
+	/* Create an anonymous mapping with all access denied */
 
-    addr = mmap(NULL, LEN, PROT_NONE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if (addr == MAP_FAILED)
-        errExit("mmap");
+	addr = mmap(NULL, LEN, PROT_NONE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	if (addr == MAP_FAILED)
+		errExit("mmap");
 
-    /* Display line from /proc/self/maps corresponding to mapping */
+	/* Display line from /proc/self/maps corresponding to mapping */
 
-    printf("Before mprotect()\n");
-    snprintf(cmd, CMD_SIZE, SHELL_FMT, (long) getpid());
-    system(cmd);
+	printf("Before mprotect()\n");
+	snprintf(cmd, CMD_SIZE, SHELL_FMT, (long) getpid());
+	system(cmd);
 
-    /* Change protection on memory to allow read and write access */
+	/* Change protection on memory to allow read and write access */
 
-    if (mprotect(addr, LEN, PROT_READ | PROT_WRITE) == -1)
-        errExit("mprotect");
+	if (mprotect(addr, LEN, PROT_READ | PROT_WRITE) == -1)
+		errExit("mprotect");
 
-    printf("After mprotect()\n");
-    system(cmd);                /* Review protection via /proc/self/maps */
+	printf("After mprotect()\n");
+	system(cmd);                /* Review protection via /proc/self/maps */
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
