c4c97e6b594e08e8bc617c298a5a23290c188606
Zhou Peng <p@ctriple.cn>
2017-07-05 12:22:29 +0800

    Linux tty manipulate.

--- lang/linux/tty/tty_functions.c
+++ lang/linux/tty/tty_functions.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU Lesser General Public License as published   *
-* by the Free Software Foundation, either version 3 or (at your option)   *
-* any later version. This program is distributed without any warranty.    *
-* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU Lesser General Public License as published   *
+ * by the Free Software Foundation, either version 3 or (at your option)   *
+ * any later version. This program is distributed without any warranty.    *
+ * See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
 \*************************************************************************/
 
 /* Listing 62-3 */
@@ -26,29 +26,28 @@
    on error. If 'prevTermios' is non-NULL, then use the buffer to which
    it points to return the previous terminal settings. */
 
-int
-ttySetCbreak(int fd, struct termios *prevTermios)
+int ttySetCbreak(int fd, struct termios *prevTermios)
 {
-    struct termios t;
+	struct termios t;
 
-    if (tcgetattr(fd, &t) == -1)
-        return -1;
+	if (tcgetattr(fd, &t) == -1)
+		return -1;
 
-    if (prevTermios != NULL)
-        *prevTermios = t;
+	if (prevTermios != NULL)
+		*prevTermios = t;
 
-    t.c_lflag &= ~(ICANON | ECHO);
-    t.c_lflag |= ISIG;
+	t.c_lflag &= ~(ICANON | ECHO);
+	t.c_lflag |= ISIG;
 
-    t.c_iflag &= ~ICRNL;
+	t.c_iflag &= ~ICRNL;
 
-    t.c_cc[VMIN] = 1;                   /* Character-at-a-time input */
-    t.c_cc[VTIME] = 0;                  /* with blocking */
+	t.c_cc[VMIN] = 1;                   /* Character-at-a-time input */
+	t.c_cc[VTIME] = 0;                  /* with blocking */
 
-    if (tcsetattr(fd, TCSAFLUSH, &t) == -1)
-        return -1;
+	if (tcsetattr(fd, TCSAFLUSH, &t) == -1)
+		return -1;
 
-    return 0;
+	return 0;
 }
 
 /* Place terminal referred to by 'fd' in raw mode (noncanonical mode
@@ -56,34 +55,33 @@ ttySetCbreak(int fd, struct termios *prevTermios)
    or -1 on error. If 'prevTermios' is non-NULL, then use the buffer to
    which it points to return the previous terminal settings. */
 
-int
-ttySetRaw(int fd, struct termios *prevTermios)
+int ttySetRaw(int fd, struct termios *prevTermios)
 {
-    struct termios t;
+	struct termios t;
 
-    if (tcgetattr(fd, &t) == -1)
-        return -1;
+	if (tcgetattr(fd, &t) == -1)
+		return -1;
 
-    if (prevTermios != NULL)
-        *prevTermios = t;
+	if (prevTermios != NULL)
+		*prevTermios = t;
 
-    t.c_lflag &= ~(ICANON | ISIG | IEXTEN | ECHO);
-                        /* Noncanonical mode, disable signals, extended
-                           input processing, and echoing */
+	t.c_lflag &= ~(ICANON | ISIG | IEXTEN | ECHO);
+	/* Noncanonical mode, disable signals, extended
+	   input processing, and echoing */
 
-    t.c_iflag &= ~(BRKINT | ICRNL | IGNBRK | IGNCR | INLCR |
-                      INPCK | ISTRIP | IXON | PARMRK);
-                        /* Disable special handling of CR, NL, and BREAK.
-                           No 8th-bit stripping or parity error handling.
-                           Disable START/STOP output flow control. */
+	t.c_iflag &= ~(BRKINT | ICRNL | IGNBRK | IGNCR | INLCR |
+		       INPCK | ISTRIP | IXON | PARMRK);
+	/* Disable special handling of CR, NL, and BREAK.
+	   No 8th-bit stripping or parity error handling.
+	   Disable START/STOP output flow control. */
 
-    t.c_oflag &= ~OPOST;                /* Disable all output processing */
+	t.c_oflag &= ~OPOST;                /* Disable all output processing */
 
-    t.c_cc[VMIN] = 1;                   /* Character-at-a-time input */
-    t.c_cc[VTIME] = 0;                  /* with blocking */
+	t.c_cc[VMIN] = 1;                   /* Character-at-a-time input */
+	t.c_cc[VTIME] = 0;                  /* with blocking */
 
-    if (tcsetattr(fd, TCSAFLUSH, &t) == -1)
-        return -1;
+	if (tcsetattr(fd, TCSAFLUSH, &t) == -1)
+		return -1;
 
-    return 0;
+	return 0;
 }
--- lang/linux/tty/ttyname.c
+++ lang/linux/tty/ttyname.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation, either version 3 or (at your option) any      *
+ * later version. This program is distributed without any warranty.  See   *
+ * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
 /* Solution for Exercise 62-2 */
@@ -29,95 +29,93 @@
    Return the device name (as a statically-allocated) string if
    found, or NULL if the device is not found or an error occurs */
 
-static char *
-ttynameCheckDir(const struct stat *fdStat, const char *devDir)
+static char *ttynameCheckDir(const struct stat *fdStat, const char *devDir)
 {
-    DIR *dirh;
-    struct dirent *dent;
-    static char *ttyPath;               /* Currently checked entry; also used
-                                           to return tty name, if found */
-    static int ttyLen = 0;              /* Length of ttyPath */
-    struct stat devStat;                /* stat entry for ttyPath */
-    int found;                          /* True if we find device entry */
-    int requiredLen;
+	DIR *dirh;
+	struct dirent *dent;
+	static char *ttyPath;               /* Currently checked entry; also used
+					       to return tty name, if found */
+	static int ttyLen = 0;              /* Length of ttyPath */
+	struct stat devStat;                /* stat entry for ttyPath */
+	int found;                          /* True if we find device entry */
+	int requiredLen;
 
-    if (ttyLen == 0) {                  /* First call - allocate ttyPath */
-        ttyPath = malloc(50);
-        if (ttyPath == NULL)
-            return NULL;
-        ttyLen = 50;
-    }
+	if (ttyLen == 0) {                  /* First call - allocate ttyPath */
+		ttyPath = malloc(50);
+		if (ttyPath == NULL)
+			return NULL;
+		ttyLen = 50;
+	}
 
-    dirh = opendir(devDir);
-    if (dirh == NULL)
-        return NULL;
+	dirh = opendir(devDir);
+	if (dirh == NULL)
+		return NULL;
 
-    /* We walk through each file in /dev looking for an entry whose
-       device ID (st_rdev) matches the device ID corresponding to 'fd'.
-       To do this, we construct a pathname for each entry in /dev, and
-       then call stat() on that pathname. This is somewhat expensive.
-       The glibc implementation of ttyname() performs an optimization:
-       it performs a first pass of the entries in /dev, performing
-       a stat() call only if fdStat->st_ino == dent->d_ino. This speeds
-       the search (since many calls to stat() are avoided), but is not
-       guaranteed to work in every case (e.g., if there are symbolic
-       links in /dev). Therefore, if the first pass fails to find a
-       matching device, glibc's ttyname() performs a second pass
-       without the st_ino check (i.e., like we do below). */
+	/* We walk through each file in /dev looking for an entry whose
+	   device ID (st_rdev) matches the device ID corresponding to 'fd'.
+	   To do this, we construct a pathname for each entry in /dev, and
+	   then call stat() on that pathname. This is somewhat expensive.
+	   The glibc implementation of ttyname() performs an optimization:
+	   it performs a first pass of the entries in /dev, performing
+	   a stat() call only if fdStat->st_ino == dent->d_ino. This speeds
+	   the search (since many calls to stat() are avoided), but is not
+	   guaranteed to work in every case (e.g., if there are symbolic
+	   links in /dev). Therefore, if the first pass fails to find a
+	   matching device, glibc's ttyname() performs a second pass
+	   without the st_ino check (i.e., like we do below). */
 
-    found = 0;
-    while ((dent = readdir(dirh)) != NULL) {
-        requiredLen = strlen(devDir) + 1 + strlen(dent->d_name) + 1;
+	found = 0;
+	while ((dent = readdir(dirh)) != NULL) {
+		requiredLen = strlen(devDir) + 1 + strlen(dent->d_name) + 1;
 
-        if (requiredLen > ttyLen) {     /* Resize ttyPath if required */
-            ttyPath = realloc(ttyPath, requiredLen);
-            if (ttyPath == NULL)
-                break;
-            ttyLen = requiredLen;
-        }
+		if (requiredLen > ttyLen) {     /* Resize ttyPath if required */
+			ttyPath = realloc(ttyPath, requiredLen);
+			if (ttyPath == NULL)
+				break;
+			ttyLen = requiredLen;
+		}
 
-        snprintf(ttyPath, ttyLen, "%s/%s", devDir, dent->d_name);
+		snprintf(ttyPath, ttyLen, "%s/%s", devDir, dent->d_name);
 
-        if (stat(ttyPath, &devStat) == -1)
-            continue;                   /* Ignore unstat-able entries */
+		if (stat(ttyPath, &devStat) == -1)
+			continue;                   /* Ignore unstat-able entries */
 
-        if (S_ISCHR(devStat.st_mode) &&
-                fdStat->st_rdev == devStat.st_rdev) {
-            found = 1;
-            break;
-        }
-    }
+		if (S_ISCHR(devStat.st_mode) &&
+		    fdStat->st_rdev == devStat.st_rdev) {
+			found = 1;
+			break;
+		}
+	}
 
-    closedir(dirh);
+	closedir(dirh);
 
-    return found ? ttyPath : NULL;
+	return found ? ttyPath : NULL;
 }
 
 /* Return the name of the terminal associated with 'fd' (in a
    statically-allocated string that is overwritten on subsequent
    calls), or NULL if it is not a terminal or an error occurs */
 
-char *
-ttyname(int fd)
+char *ttyname(int fd)
 {
-    char *d;
-    struct stat fdStat;                 /* stat entry for fd */
+	char *d;
+	struct stat fdStat;                 /* stat entry for fd */
 
-    if (!isatty(fd))                    /* Is fd even a terminal? */
-        return NULL;
+	if (!isatty(fd))                    /* Is fd even a terminal? */
+		return NULL;
 
-    if (fstat(fd, &fdStat) == -1)
-        return NULL;
+	if (fstat(fd, &fdStat) == -1)
+		return NULL;
 
-    if (!S_ISCHR(fdStat.st_mode))       /* Is fd even a character device? */
-        return NULL;
+	if (!S_ISCHR(fdStat.st_mode))       /* Is fd even a character device? */
+		return NULL;
 
-    /* First check for a pseudoterminal entry in the /dev/pts
-       directory. If that fails, try looking in /dev.
-       (We check the directories in this order for efficiency:
-       /dev/pts is small, and will contain the required device if
-       fd refers to an X-terminal or similar.)  */
+	/* First check for a pseudoterminal entry in the /dev/pts
+	   directory. If that fails, try looking in /dev.
+	   (We check the directories in this order for efficiency:
+	   /dev/pts is small, and will contain the required device if
+	   fd refers to an X-terminal or similar.)  */
 
-    d = ttynameCheckDir(&fdStat, "/dev/pts");
-    return (d != NULL) ? d : ttynameCheckDir(&fdStat, "/dev");
+	d = ttynameCheckDir(&fdStat, "/dev/pts");
+	return (d != NULL) ? d : ttynameCheckDir(&fdStat, "/dev");
 }
