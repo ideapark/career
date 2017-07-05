d1ff91af43d33a208f256ffd59ad4c83007725a3
Zhou Peng <p@ctriple.cn>
2017-07-05 12:33:49 +0800

    Linux timer API

--- lang/linux/timers/ptmr_null_evp.c
+++ lang/linux/timers/ptmr_null_evp.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation, either version 3 or (at your option) any      *
+ * later version. This program is distributed without any warranty.  See   *
+ * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
 /* Solution for Exercise 23-3 */
@@ -25,46 +25,44 @@
 #include "curr_time.h"          /* Declaration of currTime() */
 #include "tlpi_hdr.h"
 
-static void
-handler(int sig, siginfo_t *si, void *uc)
+static void handler(int sig, siginfo_t *si, void *uc)
 {
-    printf("[%s] Got signal %d\n", currTime("%T"), sig);
-    printf("    sival_int          = %d\n", si->si_value.sival_int);
+	printf("[%s] Got signal %d\n", currTime("%T"), sig);
+	printf("    sival_int          = %d\n", si->si_value.sival_int);
 #ifdef __linux__
-    printf("    si_overrun         = %d\n", si->si_overrun);
+	printf("    si_overrun         = %d\n", si->si_overrun);
 #endif
-    printf("    timer_getoverrun() = %d\n",
-            timer_getoverrun((timer_t) si->si_value.sival_ptr));
+	printf("    timer_getoverrun() = %d\n",
+	       timer_getoverrun((timer_t) si->si_value.sival_ptr));
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    struct itimerspec ts;
-    timer_t tid;
-    int j;
-    struct sigaction sa;
+	struct itimerspec ts;
+	timer_t tid;
+	int j;
+	struct sigaction sa;
 
-    if (argc < 2)
-        usageErr("%s secs [nsecs [int-secs [int-nsecs]]]\n", argv[0]);
+	if (argc < 2)
+		usageErr("%s secs [nsecs [int-secs [int-nsecs]]]\n", argv[0]);
 
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = handler;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGALRM, &sa, NULL) == -1)
-        errExit("sigaction");
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGALRM, &sa, NULL) == -1)
+		errExit("sigaction");
 
-    if (timer_create(CLOCK_REALTIME, NULL, &tid) == -1)
-        errExit("timer_create");
-    printf("timer ID = %ld\n", (long) tid);
+	if (timer_create(CLOCK_REALTIME, NULL, &tid) == -1)
+		errExit("timer_create");
+	printf("timer ID = %ld\n", (long) tid);
 
-    ts.it_value.tv_sec = atoi(argv[1]);
-    ts.it_value.tv_nsec = (argc > 2) ? atoi(argv[2]) : 0;
-    ts.it_interval.tv_sec = (argc > 3) ? atoi(argv[3]) : 0;
-    ts.it_interval.tv_nsec = (argc > 4) ? atoi(argv[4]) : 0;
-    if (timer_settime(tid, 0, &ts, NULL) == -1)
-        errExit("timer_settime");
+	ts.it_value.tv_sec = atoi(argv[1]);
+	ts.it_value.tv_nsec = (argc > 2) ? atoi(argv[2]) : 0;
+	ts.it_interval.tv_sec = (argc > 3) ? atoi(argv[3]) : 0;
+	ts.it_interval.tv_nsec = (argc > 4) ? atoi(argv[4]) : 0;
+	if (timer_settime(tid, 0, &ts, NULL) == -1)
+		errExit("timer_settime");
 
-    for (j = 0; ; j++)
-        pause();
+	for (j = 0; ; j++)
+		pause();
 }
--- lang/linux/timers/timed_read.c
+++ lang/linux/timers/timed_read.c
@@ -1,11 +1,11 @@
 /*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2016.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
+ *                  Copyright (C) Michael Kerrisk, 2016.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation, either version 3 or (at your option) any      *
+ * later version. This program is distributed without any warranty.  See   *
+ * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
 /* Listing 23-2 */
@@ -20,51 +20,50 @@
 
 #define BUF_SIZE 200
 
-static void     /* SIGALRM handler: interrupts blocked system call */
-handler(int sig)
+/* SIGALRM handler: interrupts blocked system call */
+static void handler(int sig)
 {
-    printf("Caught signal\n");          /* UNSAFE (see Section 21.1.2) */
+	printf("Caught signal\n");          /* UNSAFE (see Section 21.1.2) */
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    struct sigaction sa;
-    char buf[BUF_SIZE];
-    ssize_t numRead;
-    int savedErrno;
+	struct sigaction sa;
+	char buf[BUF_SIZE];
+	ssize_t numRead;
+	int savedErrno;
 
-    if (argc > 1 && strcmp(argv[1], "--help") == 0)
-        usageErr("%s [num-secs [restart-flag]]\n", argv[0]);
+	if (argc > 1 && strcmp(argv[1], "--help") == 0)
+		usageErr("%s [num-secs [restart-flag]]\n", argv[0]);
 
-    /* Set up handler for SIGALRM. Allow system calls to be interrupted,
-       unless second command-line argument was supplied. */
+	/* Set up handler for SIGALRM. Allow system calls to be interrupted,
+	   unless second command-line argument was supplied. */
 
-    sa.sa_flags = (argc > 2) ? SA_RESTART : 0;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_handler = handler;
-    if (sigaction(SIGALRM, &sa, NULL) == -1)
-        errExit("sigaction");
+	sa.sa_flags = (argc > 2) ? SA_RESTART : 0;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = handler;
+	if (sigaction(SIGALRM, &sa, NULL) == -1)
+		errExit("sigaction");
 
-    alarm((argc > 1) ? getInt(argv[1], GN_NONNEG, "num-secs") : 10);
+	alarm((argc > 1) ? getInt(argv[1], GN_NONNEG, "num-secs") : 10);
 
-    numRead = read(STDIN_FILENO, buf, BUF_SIZE);
+	numRead = read(STDIN_FILENO, buf, BUF_SIZE);
 
-    savedErrno = errno;                 /* In case alarm() changes it */
-    alarm(0);                           /* Ensure timer is turned off */
-    errno = savedErrno;
+	savedErrno = errno;                 /* In case alarm() changes it */
+	alarm(0);                           /* Ensure timer is turned off */
+	errno = savedErrno;
 
-    /* Determine result of read() */
+	/* Determine result of read() */
 
-    if (numRead == -1) {
-        if (errno == EINTR)
-            printf("Read timed out\n");
-        else
-            errMsg("read");
-    } else {
-        printf("Successful read (%ld bytes): %.*s",
-                (long) numRead, (int) numRead, buf);
-    }
+	if (numRead == -1) {
+		if (errno == EINTR)
+			printf("Read timed out\n");
+		else
+			errMsg("read");
+	} else {
+		printf("Successful read (%ld bytes): %.*s",
+		       (long) numRead, (int) numRead, buf);
+	}
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
