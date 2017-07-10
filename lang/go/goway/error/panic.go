13a68ae242e0329eae23b016c887ed0eeb91b978
Zhou Peng <p@ctriple.cn>
2017-07-10 12:53:28 +0800

    Remove <<the way to go>>
    Thanks to this book to bring me into the golang world.

--- lang/go/goway/error/panic.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Starting the program")
-	panic("A severe error occurred: stopping the pgoram!")
-	fmt.Println("Ending the program")
-}
--- lang/go/goway/start/hello_world.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt" // Package implementing formatted I/O.
-
-func main() {
-	fmt.Println("hello, world")
-}
