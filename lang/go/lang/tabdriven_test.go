99c00ddebe2f175d79f8d1ca872858d62d7ee251
Zhou Peng <p@ctriple.cn>
2017-07-10 21:32:59 +0800

    Golang table driven testing.

---
+++ lang/go/lang/tabdriven_test.go
@@ -0,0 +1,27 @@
+package tabdriven
+
+import (
+	"testing"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		x      int
+		y      int
+		expect int
+	}{
+		{1, 1, 2},
+		{2, 2, 4},
+		{3, 3, 6},
+	}
+	for _, tt := range tests {
+		actual := add(tt.x, tt.y)
+		if actual != tt.expect {
+			t.Errorf("add(%d,%d): expect:%d, actual:%d", tt.x, tt.y, tt.expect, actual)
+		}
+	}
+}
