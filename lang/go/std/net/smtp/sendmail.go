f105e57541909e0562b82dcaf56a6dd61c83c0ef
Zhou Peng <p@ctriple.cn>
2017-07-22 11:26:23 +0800

    use p@ctriple.cn email address

--- lang/go/std/net/smtp/sendmail.go
+++ lang/go/std/net/smtp/sendmail.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 	// Set up authentication information.
-	auth := smtp.PlainAuth("", "user@example.com", "password", "mail.example.com")
+	auth := smtp.PlainAuth("", "p@ctriple.cn", "you cannt guess", "smtp.ctriple.cn")
 
 	// Connect to the server, authenticate, set the sender and recipient,
 	// and send the email all in one step.
-	to := []string{"recipient@example.com"}
-	msg := []byte("To: recipient@example.net\r\n" +
+	to := []string{"g@ctriple.cn"}
+	msg := []byte("To: g@ctriple.cn\r\n" +
 		"Subject: discount Gophers!\r\n" +
 		"\r\n" +
 		"This is the email body.\r\n")
-	err := smtp.SendMail("mail.example.com:25", auth, "sender@example.org", to, msg)
+	err := smtp.SendMail("smtp.ctriple.cn:25", auth, "p@ctriple.cn", to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}
