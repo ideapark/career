5ff535c6986647192e0aec34a83ee373dbca6770
Zhou Peng <p@ctriple.cn>
2017-08-15 14:05:30 +0800

    golang const & string method

---
+++ lang/go/lang/const.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ByteSize float64
+
+const (
+	_           = iota // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func (b ByteSize) String() string {
+	switch {
+	case b >= YB:
+		return fmt.Sprintf("%.2fYB", b/YB)
+	case b >= ZB:
+		return fmt.Sprintf("%.2fZB", b/ZB)
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
+
+func main() {
+	datum := []ByteSize{1e5, 1e11, 1e13, 1e16, 1e17, 1e19, 1e20, 1e22, 1e23, 1e26}
+	for _, d := range datum {
+		fmt.Println(d)
+		println(d)
+	}
+}
