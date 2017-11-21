e5c7798dc2ee47757053745a37f95c23f332b548
Zhou Peng <p@ctriple.cn>
2017-11-21 19:58:24 +0800

    testcase

--- lang/eopl/chap1.scm
+++ lang/eopl/chap1.scm
@@ -1,3 +1,19 @@
+;; --------------------------------
+;; The Smaller-Subproblem Principle
+;; --------------------------------
+;; If we can reduce a problem to a smaller subproblem, we can call
+;; the procedure that solves the problem to solve the subproblem.
+
+(define-syntax equal??
+  (syntax-rules ()
+    ((_ test-exp correct-exp)
+     (let ((observed-ans test-exp))
+       (if (not (equal? observed-ans correct-exp))
+	   (printf "~s returned ~s, should have returned ~s~%"
+		   'test-exp
+		   observed-ans
+		   correct-exp))))))
+
 ;; in-S? : N → Bool
 ;; usage: (in-S? n) = #t if n is S, #f otherwise
 (define in-S?
@@ -7,11 +23,9 @@
 	    (in-S? (- n 3))
 	    #f))))
 
-;; --------------------------------
-;; The Smaller-Subproblem Principle
-;; --------------------------------
-;; If we can reduce a problem to a smaller subproblem, we can call
-;; the procedure that solves the problem to solve the subproblem.
+(equal?? (in-S? 4) #f)
+(equal?? (in-S? 9) #t)
+
 
 ;; list-length : List → Int
 ;; usage: (list-length l) = the length of l
@@ -20,6 +34,9 @@
     (if (null? lst)
 	0
 	(+ 1 (list-length (cdr lst))))))
+
+(equal?? (list-length '(a (b c) d)) 3)
+
 
 ;; nth-element : List × Int → SchemeVal
 ;; usage: (nth-element lst n) = the n-th element of lst
@@ -33,5 +50,7 @@
 
 (define report-list-too-short
   (lambda (n)
-    (eopl:error 'nth-element
-		"List too short by ~s elements.~%" (+ n 1))))
+    (error 'nth-element
+	   "List too short by ~s elements.~%" (+ n 1))))
+
+(equal?? (nth-element '(a b c d) 2) 'c)
