0e85ebdf533128f9f31f38cdeec4cf70bee0fbb5
Zhou Peng <p@ctriple.cn>
2017-11-22 13:05:51 +0800

    specifying data via interfaces

--- lang/eopl/da.scm
+++ lang/eopl/da.scm
@@ -1,1 +1,22 @@
 ;; Data Abstraction
+
+(define plus
+  (lambda (x y)
+    (if (is-zero? x)
+	y
+	(successor (plus (predecessor x y))))))
+
+;; Unary representation
+(define zero (lambda () '()))
+(define is-zero? (lambda (n) (null? n)))
+(define successor (lambda (n) (cons #t n)))
+(define predecessor (lambda (n) (cdr n)))
+
+;; Scheme number representation
+(define zero (lambda () 0))
+(define is-zero? (lambda (n) (zero? n)))
+(define successor (lambda (n) (+ n 1)))
+(define predecessor (lambda (n) (- n 1)))
+
+;; Bignum representation
+;; Page 57
