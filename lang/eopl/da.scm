9959dbbd65d69f0497fe4bde1aee765ea54b3e13
Zhou Peng <p@ctriple.cn>
2017-11-22 22:38:59 +0800

    tree parser

--- lang/eopl/da.scm
+++ lang/eopl/da.scm
@@ -52,3 +52,29 @@
 (define report-invalid-env
   (lambda (env)
     (error 'apply-env "Bad environment: ~s" env)))
+
+
+;; parse-expression : SchemeVal → LcExp
+(define parse-expression
+  (lambda (datum)
+    (cond ((symbol? datum) (var-exp datum))
+	  ((pair? datum) (if (eqv? (car datum) 'lambda)
+			     (lambda-exp
+			      (car (cadr datum))
+			      (parse-expression (caddr datum)))
+			     (app-exp
+			      (parse-expression (car datum))
+			      (parse-expression (cadr datum)))))
+	  (else (report-invalid-concrete-syntax datum)))))
+
+;; unparse-lc-exp : LcExp → SchemeVal
+(define unparse-lc-exp
+  (lambda (exp)
+    (cases lc-exp exp
+	   (var-exp (var) var)
+	   (lambda-exp (bound-var body)
+		       (list 'lambda (list bound-var)
+			     (unparse-lc-exp body)))
+	   (app-exp (rator rand)
+		    (list (unparse-lc-exp rator)
+			  (unparse-lc-exp rand))))))
