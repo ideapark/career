a02a7661425c3e7492400831d9a08711451eb3c9
Zhou Peng <p@ctriple.cn>
2017-12-09 21:22:05 +0800

    sklearn package

---
+++ refs/machinelearning/sklearn_intro.py
@@ -0,0 +1,24 @@
+# Feature Engineering
+#
+# Cluster
+#
+# 1) model architecture
+# 2) search progress
+# 3) loss estimation function
+
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+data = load_iris()
+features = data['data']
+feature_names = data['feature_names']
+target = data['target']
+
+for t, marker, c in zip(xrange(3), ">ox", "rgb"):
+    plt.scatter(features[target==t,0],
+                features[target==t,1],
+                marker=marker,
+                c=c)
+plt.grid()
+plt.show()
