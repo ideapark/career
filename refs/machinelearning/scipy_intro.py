756c1bb662e80f7ae3413fdaa9e8716e46937dfb
Zhou Peng <p@ctriple.cn>
2017-12-09 10:50:35 +0800

    SciPy introduction

---
+++ refs/machinelearning/scipy_intro.py
@@ -0,0 +1,25 @@
+import scipy, numpy
+
+print scipy.version.full_version
+
+# numpy is available by scipy
+print scipy.dot is numpy.dot
+
+# SciPy packages
+#
+# cluster
+# constants
+# fftpack
+# integrate
+# interpolate
+# io
+# linalg
+# maxentropy
+# ndimage
+# odr
+# optimize
+# signal
+# sparse
+# spatial
+# special
+# stats
