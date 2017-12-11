f9c16c60687201b7ab723a4e1f70112b967df72c
Zhou Peng <p@ctriple.cn>
2017-12-11 10:53:53 +0800

    Create output directory if not exists

--- refs/machinelearning/topic/preprocess-wikidata.sh
+++ refs/machinelearning/topic/preprocess-wikidata.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-python -m gensim.scripts.make_wiki enwiki-latest-pages-articles.xml.bz2 wiki_en_output
+if [ ! -d wiki_en_output ]; then
+   mkdir wiki_en_output
+fi
+
+python -m gensim.scripts.make_wiki enwiki-latest-pages-articles.xml.bz2 ./wiki_en_output
