3ad6104df926d2fb358e13338c2a8c29ae3c7e81
Zhou Peng <p@ctriple.cn>
2017-12-13 11:51:43 +0800

    nltk download 'punkt'

--- refs/machinelearning/nltk_intro.py
+++ refs/machinelearning/nltk_intro.py
@@ -1,7 +1,6 @@
 import nltk
 
-# Enter [all]
-nltk.download()
+nltk.download('punkt')
 
 nltk.pos_tag(nltk.word_tokenize("This is a good book."))
 nltk.pos_tag(nltk.word_tokenize("Could you please book the flight?"))
