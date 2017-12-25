cf15e72fb8c84121ab32ee5c0a1ab25441cf81e1
Zhou Peng <p@ctriple.cn>
2017-12-25 14:04:59 +0800

    more numpy operations

--- refs/machinelearning/numpy_intro.py
+++ refs/machinelearning/numpy_intro.py
@@ -57,3 +57,29 @@ s = np.array(['1', 'stringy'], dtype='|S8')
 print s.dtype
 o = np.array([1, 'stringy', set([1, 2, 3])])
 print o.dtype
+
+# zeros(), ones(), empty()
+print np.zeros((3, 4))
+print np.zeros((2, 3, 4), dtype=np.int16)
+print np.ones((2, 3))
+print np.empty((2, 3))
+
+# arange(), linspace()
+print np.arange(10, 30, 5)
+print np.arange(0, 2, 0.3)
+print np.linspace(0, 2, 9)
+print np.linspace(0, 2*np.pi, 36)
+
+# matrix product
+A = np.array([[1, 1], [0, 1]])
+B = np.array([[2, 0], [3, 4]])
+print A*B
+print A.dot(B)
+print np.dot(A, B)
+
+# return modified array
+a = np.arange(12).reshape(6, 2)
+print a.ravel()  # returns the array, flattened
+print a.T        # returns the array, transposed
+print a.T.shape
+print a.shape
