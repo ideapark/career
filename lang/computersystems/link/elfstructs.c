d2971cdef700338256a6f5b66eae3d37008a4979
Zhou Peng <p@ctriple.cn>
2018-01-31 23:12:27 +0800

    ELF symbol struct

--- lang/computersystems/link/elfstructs.c
+++ lang/computersystems/link/elfstructs.c
@@ -1,9 +1,9 @@
 typedef struct {
-	int name;  /* String table offset */
-	int value; /* Section offset, or VM address */
-	int size;  /* Object size in bytes */
+	int name;          /* String table offset */
+	int value;         /* Section offset, or VM address */
+	int size;          /* Object size in bytes */
 	char type:4,       /* Data, func, section, or src file name (4 bits) */
-		binding:4; /* Local or global (4 bits) */
+		binding:4;       /* Local or global (4 bits) */
 	char reserved;     /* Unused */
 	char section;      /* Section header index, ABS, UNDEF, Or COMMON */
 } Elf_Symbol;
@@ -11,5 +11,5 @@ typedef struct {
 typedef struct {
 	int offset;     /* Offset of the reference to relocate */
 	int symbol:24,  /* Symbol the reference should point to */
-		type:8; /* Relocation type */
+		type:8;       /* Relocation type */
 } Elf32_Rel;
