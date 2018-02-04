8ba12d1e3f833df1a7ff32bb600539202af3ee8c
Zhou Peng <p@ctriple.cn>
2018-02-04 12:02:21 +0800

    Code Review: client/server mode

--- lang/computersystems/net/netlib.c
+++ lang/computersystems/net/netlib.c
@@ -6,53 +6,58 @@
 
 int open_clientfd(char *hostname, int port)
 {
-	int clientfd;
-	struct hostent *hp;
-	struct sockaddr_in serveraddr;
+  int clientfd;
+  struct hostent *hp;
+  struct sockaddr_in serveraddr;
 
-	if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		return -1;
+  if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return -1; /* Check errno for cause of error */
 
-	if ((hp = gethostbyname(hostname)) == NULL)
-		return -2;
+  /* Fill in the server's IP address and port */
+  if ((hp = gethostbyname(hostname)) == NULL)
+    return -2; /* Check h_errno for cause of error */
 
-	bzero((char *)&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_port = htons(port);
-	bcopy((char *)hp->h_addr_list[0],
-	      (char *)&serveraddr.sin_addr.s_addr,
-	      hp->h_length);
+  bzero((char *)&serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_port = htons(port);
+  bcopy((char *)hp->h_addr_list[0],
+        (char *)&serveraddr.sin_addr.s_addr,
+        hp->h_length);
 
-	if (connect(clientfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
-		return -1;
+  /* Establish a connection with the server */
+  if (connect(clientfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+    return -1;
 
-	return clientfd;
+  return clientfd;
 }
 
 #define LISTENQ  5
 
 int open_listenfd(int port)
 {
-	int listenfd, optval = 1;
-	struct sockaddr_in serveraddr;
+  int listenfd, optval = 1;
+  struct sockaddr_in serveraddr;
 
-	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		return -1;
+  /* Create a socket descriptor */
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return -1;
 
-	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+  /* Eliminates "Address already in use" error from bind  */
+  if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
                  (const void *)&optval, sizeof(int)) < 0)
-		return -1;
+    return -1;
 
-	bzero((char *)&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons((unsigned short)port);
+  bzero((char *)&serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((unsigned short)port);
 
-	if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
-		return -1;
+  if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+    return -1;
 
-	if (listen(listenfd, LISTENQ) < 0)
-		return -1;
+  /* Make it a listening socket ready to accept connection requests */
+  if (listen(listenfd, LISTENQ) < 0)
+    return -1;
 
-	return listenfd;
+  return listenfd;
 }
