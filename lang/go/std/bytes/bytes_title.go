68036329fe3eaa48c97c33b82e8d9abdbda01e7c
Zhou Peng <p@ctriple.cn>
2018-03-06 17:45:15 +0800

    go: bytes.Title(s []byte) []byte

--- lang/go/std/bytes/bytes_title.go
+++ lang/go/std/bytes/bytes_title.go
@@ -1,3 +1,8 @@
+// Title treats s as UTF-8-encoded bytes and returns a copy with all Unicode
+// letters that begin words mapped to their title case.
+//
+// BUG(rsc): The rule Title uses for word boundaries does not handle Unicode
+// punctuation properly.
 package main
 
 import (
