3d0c2fd8d5fce57563e97fe5f82ea4c4e92d4ef5
Zhou Peng <p@ctriple.cn>
2018-03-06 17:41:07 +0800

    go: bytes.SplitAfterN(s, sep []byte, n int) [][]byte

--- lang/go/std/bytes/bytes_splitaftern.go
+++ lang/go/std/bytes/bytes_splitaftern.go
@@ -1,3 +1,10 @@
+// SplitAfterN slices s into subslices after each instance of sep and returns a
+// slice of those subslices. If sep is empty, SplitAfterN splits after each
+// UTF-8 sequence. The count determines the number of subslices to return:
+//
+// n > 0: at most n subslices; the last subslice will be the unsplit remainder.
+// n == 0: the result is nil (zero subslices)
+// n < 0: all subslices
 package main
 
 import (
