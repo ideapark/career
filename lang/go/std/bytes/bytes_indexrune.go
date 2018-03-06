06ca7c467ae059447c8bb242b3b7fb912b2e2dea
Zhou Peng <p@ctriple.cn>
2018-03-06 17:27:11 +0800

    go: bytes.IndexRune(s []byte, r rune) int

--- lang/go/std/bytes/bytes_indexrune.go
+++ lang/go/std/bytes/bytes_indexrune.go
@@ -1,3 +1,7 @@
+// IndexRune interprets s as a sequence of UTF-8-encoded code points. It returns
+// the byte index of the first occurrence in s of the given rune. It returns -1
+// if rune is not present in s. If r is utf8.RuneError, it returns the first
+// instance of any invalid UTF-8 byte sequence.
 package main
 
 import (
