c481fa21132bfa8dde6b643e1bb4cde9a4abe76b
Zhou Peng <p@ctriple.cn>
2018-03-06 17:40:26 +0800

    go: bytes.SplitAfter(s, sep []byte) [][]byte

--- lang/go/std/bytes/bytes_splitafter.go
+++ lang/go/std/bytes/bytes_splitafter.go
@@ -1,3 +1,6 @@
+// SplitAfter slices s into all subslices after each instance of sep and returns
+// a slice of those subslices. If sep is empty, SplitAfter splits after each
+// UTF-8 sequence. It is equivalent to SplitAfterN with a count of -1.
 package main
 
 import (
