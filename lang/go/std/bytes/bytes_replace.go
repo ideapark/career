b9f98f98b58a0045263244e67938fe5262c08ea9
Zhou Peng <p@ctriple.cn>
2018-03-06 17:36:52 +0800

    go: bytes.Replace(s, old, new []byte, n int) []byte

--- lang/go/std/bytes/bytes_replace.go
+++ lang/go/std/bytes/bytes_replace.go
@@ -1,3 +1,8 @@
+// Replace returns a copy of the slice s with the first n non-overlapping
+// instances of old replaced by new. If old is empty, it matches at the
+// beginning of the slice and after each UTF-8 sequence, yielding up to k+1
+// replacements for a k-rune slice. If n < 0, there is no limit on the number of
+// replacements.
 package main
 
 import (
