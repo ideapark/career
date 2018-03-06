b5de6f966dc78731cd6f0a71ab20e1a7a4e2b5ae
Zhou Peng <p@ctriple.cn>
2018-03-06 17:21:00 +0800

    go: bytes.IndexAny(s []byte, chars string) int

--- lang/go/std/bytes/bytes_indexany.go
+++ lang/go/std/bytes/bytes_indexany.go
@@ -1,3 +1,7 @@
+// IndexAny interprets s as a sequence of UTF-8-encoded Unicode code points. It
+// returns the byte index of the first occurrence in s of any of the Unicode
+// code points in chars. It returns -1 if chars is empty or if there is no code
+// point in common.
 package main
 
 import (
