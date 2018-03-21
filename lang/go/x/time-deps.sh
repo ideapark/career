5249b814627ba757dd9f0c728c579f43ea1b8c54
Zhou Peng <p@ctriple.cn>
2018-03-21 10:54:11 +0800

    go/tricks: show pakcage time dependency graphs

---
+++ lang/go/x/time-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+(
+    echo "digraph G {"
+    go list -f '{{range .Imports}}{{printf "\t%q -> %q;\n" $.ImportPath .}}{{end}}' \
+       $(go list -f '{{join .Deps " "}}' time) time
+    echo "}"
+) | dot -Tsvg -o time-deps.svg
