485c3b785280603b9758a072c4761cf7e674ea7b
Zhou Peng <p@ctriple.cn>
2018-05-30 14:48:33 +0800

    refs/machinelearning: code review, clean some warnings

--- refs/machinelearning/cluster.py
+++ refs/machinelearning/cluster.py
@@ -60,21 +60,28 @@ print(s.stem("imagine"))
 
 # NLTK stemmed CountVectorizer
 english_stemmer = nltk.stem.SnowballStemmer('english')
+
+
 class StemmedCountVectorizer(CountVectorizer):
     def build_analyzer(self):
         analyzer = super(StemmedCountVectorizer, self).build_analyzer()
         return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
+
+
 vectorizer = StemmedCountVectorizer(min_df=1, stop_words='english')
+
 print(vectorizer)
 
 # stop word boost (TF-IDF)
 import math
 import scipy as sp
 
+
 def tfidf(term, doc, docset):
     tf = float(doc.count(term))/sum(doc.count(w) for w in docset)
     idf = math.log(float(len(docset))/(len([doc for doc in docset if term in doc])))
     return tf * idf
+
 
 a, abb, abc = ["a"], ["a", "b", "b"], ["a", "b", "c"]
 D = a + abb + abc
@@ -93,10 +100,13 @@ print(tfidf("c", abc, D))
 # http://scikit-learn.org/dev/modules/clustering.html
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 class StemmedTfidfVectorizer(TfidfVectorizer):
     def build_analyzer(self):
         analyzer = super(TfidfVectorizer, self).build_analyzer()
         return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
-vectorizer = StemmedTfidfVectorizer(min_df=1,
-                                    stop_words='english')
+
+
+vectorizer = StemmedTfidfVectorizer(min_df=1, stop_words='english')
 print(vectorizer)
