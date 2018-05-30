de7aead1f6371670fb11ee0fe9e50b46f79b668e
Zhou Peng <p@ctriple.cn>
2018-05-30 14:43:46 +0800

    refs/machinelearning: fixup migration to python3

--- refs/machinelearning/sklearn_intro.py
+++ refs/machinelearning/sklearn_intro.py
@@ -8,16 +8,15 @@
 
 from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
-import numpy as np
 
 data = load_iris()
 features = data['data']
 feature_names = data['feature_names']
 target = data['target']
 
-for t, marker, c in zip(xrange(3), ">ox", "rgb"):
-    plt.scatter(features[target==t,0],
-                features[target==t,1],
+for t, marker, c in zip(range(3), ">ox", "rgb"):
+    plt.scatter(features[target == t, 0],
+                features[target == t, 1],
                 marker=marker,
                 c=c)
 plt.grid()
