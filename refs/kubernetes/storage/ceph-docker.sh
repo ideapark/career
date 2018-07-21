c6304c9e339c47fc57129acb04a25a477c4671e4
Zhou Peng <p@ctriple.cn>
2018-07-21 19:48:51 +0800

    refs/k8s: ceph bootstrap script, buggy

--- refs/kubernetes/storage/ceph-docker.sh
+++ refs/kubernetes/storage/ceph-docker.sh
@@ -18,56 +18,71 @@
 # node0
 #
 # Run our first monitor
+read cmd <<EOF
 docker run -d --net=host \
        -v /etc/ceph:/etc/ceph \
        -v /var/lib/ceph:/var/lib/ceph \
        -e MON_IP=192.168.99.101 \
        -e CEPH_PUBLIC_NETWORK=192.168.99.0/24 \
        ceph/daemon mon
+EOF
+ssh -t node0 $cmd
 
 # node0
 #
 # Distribute /etc/ceph,/var/lib/ceph to node1,node2,node3
 #
 # NOTE: root permission required
+read cmd <<EOF
 for node in node1 node2 node3
 do
     scp -r /etc/ceph     ${node}:/etc
     scp -r /var/lib/ceph ${node}:/var/lib
 done
+EOF
+ssh -t node0 $cmd
 
 # node1
 #
 # Run second monitor
+read cmd <<EOF
 docker run -d --net=host \
        -v /etc/ceph:/etc/ceph \
        -v /var/lib/ceph:/var/lib/ceph \
        -e MON_IP=192.168.99.102 \
        -e CEPH_PUBLIC_NETWORK=192.168.99.0/24 \
        ceph/daemon mon
+EOF
+ssh -t node1 $cmd
 
 # node2
 #
 # Run third monitor
+read cmd <<EOF
 docker run -d --net=host \
        -v /etc/ceph:/etc/ceph \
        -v /var/lib/ceph:/var/lib/ceph \
        -e MON_IP=192.168.99.103 \
        -e CEPH_PUBLIC_NETWORK=192.168.99.0/24 \
        ceph/daemon mon
+EOF
+ssh -t node2 $cmd
 
 # node0
 #
 # Run a ceph manager
+read cmd <<EOF
 docker run -d --net=host \
        -v /etc/ceph:/etc/ceph \
        -v /var/lib/ceph:/var/lib/ceph \
        ceph/daemon mgr
+EOF
+ssh -t node0 $cmd
 
 # node0,node1,node2,node3
 #
 # Run 2 osds (Object Storage Daemon) on each node
-
+read cmd <<EOF
 # destroy partition table if needed
 docker run --rm -d --privileged=true \
        -v /dev:/dev \
@@ -99,34 +114,51 @@ docker run -d --net=host \
        -v /var/lib/ceph:/var/lib/ceph \
        -e OSD_DEVICE=/dev/sdc \
        ceph/daemon osd
+EOF
+for node in node0 node1 node2 node3
+do
+    ssh -t ${node} ${cmd}
+done
 
 # node2,node3
 #
 # Run 2 mds (Metadata Server)
+read cmd <<EOF
 docker run -d --net=host \
        -v /etc/ceph:/etc/ceph \
        -v /var/lib/ceph:/var/lib/ceph \
        -e CEPHFS_CREATE=1 \
        ceph/daemon mds
+EOF
+for node in node2 node3
+do
+    ssh -t ${node} ${cmd}
+done
 
 # node1
 #
 # Run a rgw (Rados Gateway)
 # NOTE: expose container port 8080 to host 80 port
+read cmd <<EOF
 docker run -d -p 80:8080 \
        -v /etc/ceph:/etc/ceph \
        -v /var/lib/ceph:/var/lib/ceph \
        ceph/daemon rgw
+EOF
+ssh -t node1 $cmd
+
+exit 0
 
 # DEBUG:
 #   stop and remove ceph containers
 #   delete /etc/ceph,/var/lib/ceph
+read cmd <<EOF
+docker stop \$(docker ps -aq)
+docker rm   \$(docker ps -aq)
+rm -rf /etc/ceph
+rm -rf /var/lib/ceph
+EOF
 for node in node0 node1 node2 node3
 do
-    ssh -t ${node} <<EOF
-        docker stop \$(docker ps -aq)
-        docker rm   \$(docker ps -aq)
-        rm -rf /etc/ceph
-        rm -rf /var/lib/ceph
-EOF
+    ssh -t ${node} ${cmd}
 done
