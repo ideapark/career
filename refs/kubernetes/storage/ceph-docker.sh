ab2e618d5bc790dd79da2c2eeb7fb1b48d5a8f97
Zhou Peng <p@ctriple.cn>
2018-07-21 23:16:05 +0800

    Revert "refs/k8s: ceph bootstraping: separate two osds bootstraping."
    This reverts commit e337667520537ecb033e3c4a51c7bf7e3755b40f.
    
    This was not a problem.

--- refs/kubernetes/storage/ceph-docker.sh
+++ refs/kubernetes/storage/ceph-docker.sh
@@ -97,9 +97,8 @@ read -p 'confirm mgr initialized ok, continue? '
 # node0,node1,node2,node3
 #
 # Run 2 osds (Object Storage Daemon) on each node
-
-# ===> osd on /dev/sdb
 (cat <<'EOF'
+# ===> osd on /dev/sdb
 docker run -d --net=host \
        --pid=host \
        --privileged=true \
@@ -108,17 +107,8 @@ docker run -d --net=host \
        -v /var/lib/ceph/:/var/lib/ceph/ \
        -e OSD_DEVICE=/dev/sdb \
        ceph/daemon osd
-EOF
-) > $CMD
-for node in node0 node1 node2 node3
-do
-    ssh -t ${node} 'bash -s' < $CMD
-done
-
-read -p 'confirm osd on [/dev/sdb] initialized ok, continue? '
 
 # ===> osd on /dev/sdc
-(cat <<'EOF'
 docker run -d --net=host \
        --pid=host \
        --privileged=true \
@@ -134,7 +124,7 @@ do
     ssh -t ${node} 'bash -s' < $CMD
 done
 
-read -p 'confirm osd on [/dev/sdc] initialized ok, continue? '
+read -p 'confirm osds initialized ok, continue? '
 
 # node2,node3
 #
