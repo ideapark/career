a3c3773e37f9b6f6b56726939a0168608804e4a3
Zhou Peng <p@ctriple.cn>
2018-07-25 10:02:36 +0800

    refs/concurrency: thread lock contention

--- refs/concurrency/ThreadLocks/HelloWorld/src/main/java/com/paulbutcher/HelloWorld.java
+++ refs/concurrency/ThreadLocks/HelloWorld/src/main/java/com/paulbutcher/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) throws InterruptException {
+    public static void main(String[] args) throws InterruptedException {
         Thread myThread = new Thread() {
                 public void run() {
                     System.out.println("Hello from new thread");
