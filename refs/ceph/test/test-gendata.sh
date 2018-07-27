dcff90722f405e77fd8145c0e7fdd5f386f55556
Zhou Peng <p@ctriple.cn>
2018-07-28 00:29:14 +0800

    ceph: generate test data file named prefix with 'test-'

--- refs/ceph/test/test-gendata.sh
+++ refs/ceph/test/test-gendata.sh
@@ -4,8 +4,8 @@
 
 test -d testdata || mkdir testdata
 
-for size in 1k 10k 1m 10m 100m 500m 1g
+for size in 1K 10K 1M 10M 100M 500M 1G
 do
     echo "GENDATA ===>> $size"
-    head -c $size < /dev/urandom > testdata/${size}.dat
+    head -c $size < /dev/urandom > testdata/test-${size}.dat
 done
