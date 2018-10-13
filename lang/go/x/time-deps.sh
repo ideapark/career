9f20e133b9acba5469b5cd6018bdf017a66c3731
Zhou Peng <p@ctriple.cn>
2018-10-13 16:58:47 +0800

    lang/go: these were go's tricks, i do not need any more.
    Signed-off-by: Zhou Peng <p@ctriple.cn>

--- lang/go/x/time-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-(
-    echo "digraph G {"
-    go list -f '{{range .Imports}}{{printf "\t%q -> %q;\n" $.ImportPath .}}{{end}}' \
-       $(go list -f '{{join .Deps " "}}' time) time
-    echo "}"
-) | dot -Tsvg -o time-deps.svg
