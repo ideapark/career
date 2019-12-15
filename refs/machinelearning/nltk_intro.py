6515d26e869bf98fad554f7cd0b2ff14df365d39
Zhou Peng <p@ctriple.cn>
2019-12-15 23:15:43 +0800

    [machinelearning]: goodbye
    Signed-off-by: Zhou Peng <p@ctriple.cn>

--- refs/machinelearning/nltk_intro.py
+++
@@ -1,6 +0,0 @@
-import nltk
-
-nltk.download('punkt')
-
-nltk.pos_tag(nltk.word_tokenize("This is a good book."))
-nltk.pos_tag(nltk.word_tokenize("Could you please book the flight?"))
--- refs/machinelearning/topic/preprocess-wikidata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -d wiki_en_output ]; then
-   mkdir wiki_en_output
-fi
-
-python -m gensim.scripts.make_wiki enwiki-latest-pages-articles.xml.bz2 ./wiki_en_output
