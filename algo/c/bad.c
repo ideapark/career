4605e4e3592c1a1918ace1d2a9b8634b17219cae
Zhou Peng <p@ctriple.cn>
2019-12-27 14:23:35 +0800

    [algo/c]: drop naive ones
    Signed-off-by: Zhou Peng <p@ctriple.cn>

--- algo/c/bad.c
+++
@@ -1,7 +0,0 @@
-int Bad(unsigned int N)
-{
-	if (N == 0)
-		return 0;
-	else
-		return Bad(N / 3 + 1) + N - 1;
-}
--- algo/c/print.c
+++
@@ -1,9 +0,0 @@
-extern int PrintDigit(int i);
-
-/* Print nonnegative N */
-void PrintOut(unsigned int N)
-{
-	if (N >= 10)
-		PrintOut(N / 10);
-	PrintDigit(N % 10);
-}
