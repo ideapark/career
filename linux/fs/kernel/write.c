394bbbfd1588733ac864fcc4b1992a0be7b488b1
Zhou Peng <p@ctriple.cn>
2020-02-23 23:20:33 +0800

    [all]: syscall
    Signed-off-by: Zhou Peng <p@ctriple.cn>

--- linux/fs/kernel/write.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int main(void)
-{
-        if ((write(1, "Here is some data\n", 18)) != 18)
-                write(2, "A write error has occurred on file descriptor 1\n", 48);
-        return 0;
-}
--- linux/mm/read-zero.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-        char z = *(char *)0;
-        printf("I read from location zero\n");
-        return 0;
-}
