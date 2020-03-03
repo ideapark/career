bae2f747aaadb95c38801636cf65b22ce8ebd306
Zhou Peng <p@ctriple.cn>
2020-03-03 11:41:58 +0800

    [kernel]: temporary file descriptor
    Signed-off-by: Zhou Peng <p@ctriple.cn>

--- kernel/tmpfd.sh
+++ kernel/tmpfd.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-set -x                       # turn on execution tracing
-diff -y <(ls -lF) <(ls -l)   # run command
-set +x                       # turn off execution tracing
+cat <<EOF
+ $ diff -y <(ls -lF) <(ls -l)
 
-echo ''
-echo '################## PROCESS SUBSTITUTION ####################'
-echo '                                                            '
-echo '+------+             |                                      '
-echo '|ls -lF| -----> fd 1 | fd 63 ---------------+               '
-echo '+------+            Pipe                    |               '
-echo '                                            v               '
-echo '                             +----------------------------+ '
-echo '                             | diff /dev/fd/63 /dev/fd/62 | '
-echo '                             +----------------------------+ '
-echo '                                                       ^    '
-echo '+-----+             Pipe                               |    '
-echo '|ls -l| ------> fd 1 | fd 62 --------------------------+    '
-echo '+-----+              |                                      '
-echo ''
+ +------+             |
+ |ls -lF| -----> fd 1 | fd 63 ---------------+
+ +------+            Pipe                    |
+                                             v
+                              +----------------------------+
+                              | diff /dev/fd/63 /dev/fd/62 |
+                              +----------------------------+
+                                                        ^
+ +-----+             Pipe                               |
+ |ls -l| ------> fd 1 | fd 62 --------------------------+
+ +-----+              |
+
+EOF
+
+# temporary file descriptor
+diff -y <(ls -lF) <(ls -l)
