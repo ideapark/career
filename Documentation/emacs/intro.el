8ca4230be0774eb860a32bb339691419a255e398
Park Zhou <buildpaas@gmail.com>
2023-04-11 23:36:02 +0800

    emacs: error message
    Signed-off-by: Park Zhou <buildpaas@gmail.com>

--- Documentation/emacs/intro.el
+++ Documentation/emacs/intro.el
@@ -23,3 +23,20 @@ t        ; true
 nil      ; nil (false, empty, ...)
 
 '(this list includes "text between quotation marks.")
+
+;; error message
+(this list includes "text between quotation marks.") ;; C-x C-e
+;;
+;; Debugger entered--Lisp error: (void-function this)
+;;   (this list includes "text between quotation marks.")
+;;   eval((this list includes "text between quotation marks.") nil)
+;;   elisp--eval-last-sexp(nil)
+;;   eval-last-sexp(nil)
+;;   funcall-interactively(eval-last-sexp nil)
+;;   call-interactively(eval-last-sexp nil nil)
+;;   command-execute(eval-last-sexp)
+
+;; this is an emacs lisp atom (vector)
+[this list includes "text between quotation marks."] ;; C-x C-e
+
+
