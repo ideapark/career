e63b30da57d56b3ad5f9492f0028a1299a529291
Park Zhou <buildpaas@gmail.com>
2023-04-11 23:23:12 +0800

    emacs: emacs lisp introduction
    Signed-off-by: Park Zhou <buildpaas@gmail.com>

---
+++ Documentation/emacs/intro.el
@@ -0,0 +1,25 @@
+;;; Lisp Lists
+
+;; flowers
+'(rose violet daisy buttercup)
+
+;; flowers (exactly the same with above)
+'(rose
+  violet
+  daisy
+  buttercup)
+
+;; numbeers
+(+ 2 2)
+
+;; list inside of list
+'(this list has (a list inside of it))
+
+;; atom
+2        ; numbeer
+"lisp"   ; string
+?c       ; char
+t        ; true
+nil      ; nil (false, empty, ...)
+
+'(this list includes "text between quotation marks.")
